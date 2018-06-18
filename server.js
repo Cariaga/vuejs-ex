a5511e2213e4e8a46de895db93361f25fe45b27d
Kaivaan <cariaga.info@gmail.com>
2018-06-18 22:01:05 +0800

    asf

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 
-
-app.use(app.router);//required by post request
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
