577c613e7cb0714aa5f9c0f38dfc7561f824165e
Kaivaan <cariaga.info@gmail.com>
2018-06-21 14:01:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -148,10 +148,11 @@ let WithdrawHistory = mongoose.Schema({UserID: 'String',
 
 //to enable CORS required to
 app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setheader("Access-Control-Allow-Origin", "*");
+  res.setheader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setheader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setheader("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader('Access-Control-Allow-Credentials', true)
   next();
 });
 
