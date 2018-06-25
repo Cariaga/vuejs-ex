f6bb2e3141255f5381433193da7faf083047181b
Kaivaan <cariaga.info@gmail.com>
2018-06-25 20:03:14 +0800

    test

--- Models/Models.js
+++ Models/Models.js
@@ -1,5 +1,10 @@
 const mysql = require('mysql2');
 const Sequelize = require('sequelize');
+const  sequelize = new Sequelize('sampledb', 'user', 'user', {
+  host:'172.30.166.206',
+  port: 3306,
+  dialect: 'mysql'
+});
 //model
  var Item = sequelize.define('Item', {
     id: {
