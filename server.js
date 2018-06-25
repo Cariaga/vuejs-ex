4fd29d79b147b10933886655f80dd707e9f753f4
Kaivaan <cariaga.info@gmail.com>
2018-06-25 16:26:01 +0800

    asf

--- server.js
+++ server.js
@@ -344,8 +344,8 @@ app.get('/Api/v1/SupportTicket/Update/:UserAccountID/:Title/:Description/:Reason
 app.get('/Api/v1/SupportTicket', function (req, res) {
   res.send("SupportTicket");
 });
-app.get('/Api/v1/SupportTicket/Offset?Limit?Sort?/', function (req, res) {
-  res.send("Un Implemented");
+app.get('/Api/v1/SupportTicket?Offset?Limit?Sort/', function (req, res) {
+  res.send("Un SupportTicket?Offset?Limit?Sort");
 });
 
 
