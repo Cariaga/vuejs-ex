13c62de85581953c20cb8116800e0a4160261381
Kaivaan <cariaga.info@gmail.com>
2018-06-25 17:19:14 +0800

    asf

--- server.js
+++ server.js
@@ -342,8 +342,8 @@ app.get('/Api/v1/SupportTicket/Update/:UserAccountID/:Title/:Description/:Reason
 
 });
 
-app.get('/Api/v1/SupportTicket', function (req, res) {
-  res.send("SupportTicket" +res.query.Offset +" "+res.query.Limit+" "+res.query.Limit);
+app.get('/Api/v1/SupportTicket/?Offset?Limit?Sort', function (req, res) {
+  res.send("SupportTicket" +res.query.Offset +" "+res.query.Limit+" "+res.query.Sort);
 });
 
 
