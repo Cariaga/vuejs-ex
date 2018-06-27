b814f4d27a820365804d007f02b5e4df5979f414
Kaivaan <cariaga.info@gmail.com>
2018-06-27 15:16:59 +0800

    asf

--- server.js
+++ server.js
@@ -135,6 +135,7 @@ app.get('/Api/v1/Login/:UserName/:Password/', function (req, res) {
 //---API Login End
 //---SupportTicket ROUTING START
 app.get('/Api/v1/SupportTicket/Add/:UserAccountID/:Title/:Description/:Reason/:Time/:Date/:Status', function (req, res) {
+  //
   let UserAccountID = req.params.UserAccountID;
   let Title = req.params.Title;
   let Description = req.params.Description;
@@ -158,7 +159,7 @@ app.get('/Api/v1/SupportTicket/Add/:UserAccountID/:Title/:Description/:Reason/:T
       Date:Date,
       Status:Status
     });
-    Models.SupportTicket.sync({alter : true,force:true});//force to recreate if non production code
+    Models.SupportTicket.sync({alter : true});//force to recreate if non production code
     item1.save()
     .then(Success => {
       res.send("Inserted");
