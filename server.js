8b104bd01dc3c4be593543e742bd48850fe6fd1d
Kaivaan <cariaga.info@gmail.com>
2018-06-27 19:25:26 +0800

    asf

--- server.js
+++ server.js
@@ -1370,7 +1370,7 @@ app.get('/Api/v1/Distributor', function (req, res) {
           
       });
      
-      //res.send(beautify(Data, null, 2, 100));
+      res.send(beautify(Data, null, 2, 100));
     }).catch(function(result) {//catching any then errors
 
       res.send("Error "+result);
