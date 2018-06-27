ced40186afb114c4227741716b2f868212d04d26
Kaivaan <cariaga.info@gmail.com>
2018-06-27 15:07:10 +0800

    asf

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ app.get('/Api/v1/SupportTicket', function (req, res) {
           
       });
      
-      //res.send(beautify(Data, null, 2, 100));
+      res.send(beautify(Data, null, 2, 100));
     }).catch(function(result) {//catching any then errors
 
       res.send("Error "+result);
