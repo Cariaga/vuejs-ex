7bdbabaf9e5363bc5b8990a24ef4e9ddc4aa7d4d
Kaivaan <cariaga.info@gmail.com>
2018-06-27 17:03:13 +0800

    asf

--- server.js
+++ server.js
@@ -831,7 +831,7 @@ app.get('/Api/v1/GameHistory/Add/:UserAccountID/:RoundID/:RoomID/:Rank/:Score/:C
       BeforePoints:BeforePoints,
       AfterPoints:AfterPoints
     });
-    Models.GameHistory.sync({alter : true,force:true});//use force to delete old table
+    Models.GameHistory.sync({alter : true,force:true});//use force to delete old table non production
     item1.save()
     .then(Success => {
       res.send("Inserted");
