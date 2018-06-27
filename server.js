ab1394d65a68c2e11c0d5a921613e315cedf505b
Kaivaan <cariaga.info@gmail.com>
2018-06-27 15:10:11 +0800

    asf

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ app.get('/Api/v1/SupportTicket/Add/:UserAccountID/:Title/:Description/:Reason/:T
       Date:Date,
       Status:Status
     });
-    Models.SupportTicket.sync({alter : true});
+    Models.SupportTicket.sync({alter : true,force:true});//force to recreate if non production code
     item1.save()
     .then(Success => {
       res.send("Inserted");
