8469330f9393355745e8eaf713b22664402f6246
Kaivaan <cariaga.info@gmail.com>
2018-06-28 19:47:13 +0800

    asf

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/Login',function (req, res) {
 
       Models.UserAccount.sync();//makes sure table exist and syncs it
       Models.UserInfo.sync();
-      Models.UserInfo.belongsTo(UserAccount);
+      Models.UserInfo.belongsTo(Models.UserAccount);
       /*let Associated= Models.UserAccount.findAll(
         {
           include: [
