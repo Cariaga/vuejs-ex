0b2deeef44ac8649a50504010cb3d7bbb195005e
Kaivaan <cariaga.info@gmail.com>
2018-06-28 21:18:31 +0800

    asf

--- server.js
+++ server.js
@@ -101,9 +101,10 @@ app.get('/Login',function (req, res) {
   if(!isNullOrEmpty(UserName)){
     if(!isNullOrEmpty(Password)){
 
+      Models.UserAccount.sync({force:true});//makes sure table exist and syncs it
+      Models.UserInfo.sync({force:true});
       Models.UserAccount.sync();//makes sure table exist and syncs it
       Models.UserInfo.sync();
-
       //Models.UserInfo.belongsTo(Models.UserAccount, {foreignKey: 'UserAccountID'});
       Models.UserInfo.belongsTo(Models.UserAccount, {foreignKey: 'UserAccountID', targetKey: 'UserAccountID'});
       let Associated= Models.UserAccount.findAll(
