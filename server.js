40f18c0f4a3bdd731ad5865076905eca5b8781fa
Kaivaan <cariaga.info@gmail.com>
2018-07-04 01:34:57 +0800

    asf

--- server.js
+++ server.js
@@ -42,14 +42,14 @@ app.use(function (req, res, next) {
   var origin = req.headers.origin;
   res.header("Access-Control-Allow-Origin", "*");
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
