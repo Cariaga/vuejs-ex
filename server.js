a85f8a79ca84450db19a65d8f114bdb394ccd43a
Kaivaan <cariaga.info@gmail.com>
2018-07-03 16:18:03 +0800

    asf

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/SMS/:recipient/:message', function (req, res){
   });
   }
   res.send("Sent all to " +recipient);*/
-  console.log(req.params.recipient);
+ // console.log(req.params.recipient);
   res.end();
 });
 
