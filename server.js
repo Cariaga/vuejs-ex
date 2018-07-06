ebace74ae8c5b0147bb5fc39e36f134691974b1c
Kaivaan <cariaga.info@gmail.com>
2018-07-06 23:17:23 +0800

    asf

--- server.js
+++ server.js
@@ -2106,7 +2106,7 @@ app.get('/Api/v1/UserAccount/Clear', function (req, res){// will not work due to
 app.get('/Api/v1/UserAccount/Delete', function (req, res){
   //will not execute if has FK set Up
   
-  Models.UserInfo.sync()
+  Models.UserInfo.sync({force:true})
   .then(function(result) {
     sequelize.queryInterface.removeConstraint("UserInfo", "UserAccountID")
     .then(function(result) {
@@ -2117,7 +2117,6 @@ app.get('/Api/v1/UserAccount/Delete', function (req, res){
     }).catch(function(error){
       console.log("UserInfo > UserAccount RemoveConstraint Error");
     });
-  
   }).catch(function(result) {//catching any then errors
     console.log("UserInfo Error : "+result);
   });
