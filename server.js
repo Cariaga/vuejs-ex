6c325f0430281f89f045fa82f151d0ba27e3abb6
Kaivaan <cariaga.info@gmail.com>
2018-07-06 22:37:16 +0800

    asf

--- server.js
+++ server.js
@@ -2105,7 +2105,7 @@ app.get('/Api/v1/UserAccount/Clear', function (req, res){// will not work due to
 });
 app.get('/Api/v1/UserAccount/Delete', function (req, res){
   //will not execute if has FK set Up
-  
+  /*
   Models.UserInfo.sync({force:true})
   .then(function(result) {
     sequelize.queryInterface.removeConstraint("UserInfo", "UserAccountID");//Removes all The connection from UserAccount
@@ -2113,15 +2113,15 @@ app.get('/Api/v1/UserAccount/Delete', function (req, res){
   
   }).catch(function(result) {//catching any then errors
     console.log("UserInfo Error : "+result);
-  });
-  /*Models.UserAccount.sync({force:true})
+  });*/
+  Models.UserAccount.sync({force:true})
   .then(function(result) {
     sequelize.queryInterface.removeConstraint("UserAccount", "UserAccountID");//Removes all The connection from UserAccount
     sequelize.queryInterface.removeConstraint("UserInfo", "UserAccountID");//Removes all The connection from UserAccount
   }).catch(function(result) {//catching any then errors
     console.log(result);
     res.send("UserAccount Error "+result);
-  });*/
+  });
 
   res.send("Deleted2");
 });
