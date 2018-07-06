87c5ef247c2b8a3a2a5fb57ef4b4debc06e470ab
Kaivaan <cariaga.info@gmail.com>
2018-07-06 21:53:16 +0800

    asf

--- server.js
+++ server.js
@@ -2109,23 +2109,21 @@ app.get('/Api/v1/UserAccount/Delete', function (req, res){
   Models.UserInfo.sync({force:true})
   .then(function(result) {
     sequelize.queryInterface.removeConstraint("UserInfo", "UserAccountID");//Removes all The connection from UserAccount
-    sequelize.queryInterface.removeConstraint("UserAccount", "UserAccountID");//Removes all The connection from UserAccount
-    res.send("Deleted");
+    //sequelize.queryInterface.removeConstraint("UserAccount", "UserAccountID");//Removes all The connection from UserAccount
   }).catch(function(result) {//catching any then errors
     console.log(result);
   });
 
- /* Models.UserAccount.sync({force:true})
+  Models.UserAccount.sync({force:true})
   .then(function(result) {
-   
-    sequelize.queryInterface.removeConstraint("UserInfo", "UserAccountID");//Removes all The connection from UserAccount
+    //sequelize.queryInterface.removeConstraint("UserInfo", "UserAccountID");//Removes all The connection from UserAccount
     sequelize.queryInterface.removeConstraint("UserAccount", "UserAccountID");//Removes all The connection from UserAccount
-
-    res.send("Deleted2");
   }).catch(function(result) {//catching any then errors
     console.log(result);
     res.send("Error "+result);
-  });*/
+  });
+  
+  res.send("Deleted2");
 });
 app.get('/Api/v1/UserAccount', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
