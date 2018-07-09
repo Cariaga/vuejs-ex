9ada409e23767675bceff5c0aaffdaef9483e11f
Kaivaan <cariaga.info@gmail.com>
2018-07-09 21:58:13 +0800

    asf

--- server.js
+++ server.js
@@ -525,7 +525,7 @@ function Verify(UserName,ValidKey,callback){
        ], function (err, result) {//final function
            // result now equals 'done'
            console.log('5');
-           callback(null,result);
+           callback(result);
        });
         function myFirstFunction(callback2) {
           console.log('1');
@@ -538,24 +538,28 @@ function Verify(UserName,ValidKey,callback){
            }
           }).then(function(result) {
             let Data = result.map(function(item) {return item;});
+            console.log('2');
             callback2(null,Data);
-          }).catch(function(result){
-            console.log("Verify Error : "+result);
-            callback2(null,result);
+          }).catch(function(result2){
+            console.log("Verify Error : "+result2);
+            console.log('2');
+            callback2(null,result2);
           });
-          console.log('2');
+       
         }
        function mySecondFunction(arg1,callback3) {
       //  console.log(arg1);
         console.log('3');
         if(arg1.length==1){
-          let result = {Verified:true};
-          callback3(null,result);
+          let result3 = {Verified:true};
+          console.log('4');
+          callback3(null,result3);
         }else{
-          let result = {Verified:false};
-          callback3(null,result);
+          let result3 = {Verified:false};
+          console.log('4');
+          callback3(null,result3);
         }
-        console.log('4');
+      
         }
 
       
