54da9e6dd0cec6697a63a3224022cb1dd81098e5
Kaivaan <cariaga.info@gmail.com>
2018-07-16 23:57:27 +0800

    asf

--- server.js
+++ server.js
@@ -519,7 +519,7 @@ app.get('/Login',function (req, res) {
                   AddLoginHistory(UserAccountID,IP,DeviceName,DeviceRam,DeviceCpu,Time,Date,function(response3){
                     console.log('6');
                     console.obj(response3);
-                    let Data =[ {Status:"Verified",
+                    let Data =[{Status:"Verified",
                     Controller:"/Login",
                     UserAccountID:UserAccountID,
                     Solution:"No Issue",
@@ -536,7 +536,7 @@ app.get('/Login',function (req, res) {
                   //--Validation For Login End
         
                 }else{
-                  res.send([{
+                  let Data = [{
                     Status:"Unverified",
                     Controller:"/Login",
                     Solution:"Check Mail For Verification",
@@ -547,7 +547,8 @@ app.get('/Login',function (req, res) {
                     ContactNumber:"",
                     AccessType:""
                     
-                  }]);
+                  }];
+                  res.send(Data);
                 }
                 //res.send(beautify(Data, null, 2, 100));
         
