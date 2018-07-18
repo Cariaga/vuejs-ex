c7061e02a30c46599ba4da4ed4b9240b66a583a9
Kaivaan <cariaga.info@gmail.com>
2018-07-18 15:00:16 +0800

    asf

--- server.js
+++ server.js
@@ -240,10 +240,16 @@ function isDistributorAlreadyExist(DistributorID,callback){
       let Data = result.map(function(item) {
           return item;
       });
-      callback(Data);
+      if(Data.length>0){
+        callback(Data);
+      }else{
+        callback(undefined);
+      }
+     
      // res.send(beautify(Data, null, 2, 100));
     }).catch(function(result) {//catching any then errors
-      callback(result);
+      console.log(result);
+      callback(undefined);
     });
 }
 
