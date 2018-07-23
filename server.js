7e6c1101d2ffa98ed9230c4ca1b1bcb516933862
Kaivaan <cariaga.info@gmail.com>
2018-07-23 23:40:46 +0800

    asf

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var beautify = require("json-beautify");
 var uuidv4 = require('uuid/v4');
 var passwordValidator = require('password-validator');
 var validator = require('validator');//email,mobile phone,isIP,isPostalCode,credit card
-var Enumerable = require('linq');
+var Enumerable = require('../linq');
 const sendmail = require('sendmail')();
 
 require("./routes/test")(app);
