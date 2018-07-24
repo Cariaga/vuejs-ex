07a63d31754dce360da8c6f08cae776ca4289edd
Kaivaan <cariaga.info@gmail.com>
2018-07-24 20:35:10 +0800

    asf

--- server.js
+++ server.js
@@ -3421,7 +3421,7 @@ app.get('/Api/v1/UserAccount/AccountType/:UserAccountID', function (req, res) {
   let UserAccountID = req.params.UserAccountID;
   if(!isNullOrEmpty(UserAccountID)){
     AccountTypeFullCheck(UserAccountID,function(response){
-      res.send(response);
+   
       if(isNullOrEmpty(response)&&response.UnSafeDuplicate==false&&response.FoundAccount==true){
         res.send("Valid");
       }
