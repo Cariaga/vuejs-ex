bd859a766c0c4d9a7a3250e649b4b90456ad9965
Kaivaan <cariaga.info@gmail.com>
2018-07-24 17:13:59 +0800

    asf

--- server.js
+++ server.js
@@ -3419,10 +3419,9 @@ app.get('/Api/v1/UserAccount/AccountType/:UserAccountID', function (req, res) {
       if(!isNullOrEmpty(response)&&response.length>0&&response!=undefined&&response.UnSafeDuplicate!=undefined&&response.UnSafeDuplicate==false){
         res.send(response);
       }
-      else if(isNullOrEmpty(response)&&response.length>0&&response!=undefined&&response.UnSafeDuplicate!=undefined&&response.UnSafeDuplicate==true){
-        
+      else if(!isNullOrEmpty(response)&&response.length>0&&response!=undefined&&response.UnSafeDuplicate!=undefined&&response.UnSafeDuplicate==true){
+
       }
-      
       else if(isNullOrEmpty(response)&&response==undefined&&response.UnSafeDuplicate==undefined){
         res.send([]);
       }
@@ -3449,9 +3448,8 @@ function AccountTypeFullCheck(UserAccountID,callback){//this is an application l
       callback(result);
 
     }else if(TotalTrue>1){
-      let ERROR = {ERROR:'ERROR TWO Accounts UserAccountID Should not Exist in Two OR More tables in SHOP HEADOFFICE DISTRIBUTOR PLAYER'};
+      let ERROR = {ERROR:'ERROR TWO Accounts UserAccountID Should not Exist in Two OR More tables in SHOP HEADOFFICE DISTRIBUTOR PLAYER',UnSafeDuplicate:true};
       console.log(ERROR + " RESULT "+Data);
-      ERROR.UnSafeDuplicate =true;
       callback(ERROR);
     }
     else if(TotalTrue<1){
