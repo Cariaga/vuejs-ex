8943d9b9e34735bdcde4d8ff1433427a6bab4767
Kaivaan <cariaga.info@gmail.com>
2018-07-27 15:32:56 +0800

    asf

--- server.js
+++ server.js
@@ -4015,7 +4015,6 @@ function AddPlayer(UserAccountID,ShopID,ScreenName,Name,Surname,CurrentRoomName,
 app.get('/Api/v1/Player/Update/UserAccountID/:UserAccountID/Add/Point/:Point', function (req, res) {
   let UserAccountID = req.params.UserAccountID;
   let Point = req.params.Point;
-
   if(!isNullOrEmpty(UserAccountID)){
     if(!isNullOrEmpty(Point)){
       if(validator.isInt(Point)==true){
@@ -4023,6 +4022,73 @@ app.get('/Api/v1/Player/Update/UserAccountID/:UserAccountID/Add/Point/:Point', f
         let CurrentPoints = undefined;
         async.series([UserAccountIDCheck,PlayerCurrentPointsCheck],function(error,response){
           let NewPoints = parseInt(CurrentPoints)+parseInt(Point);
+          
+          if(UserAccountIDExist==true){
+            res.send({NewPoints:NewPoints});
+            PlayerUpdatePoint(UserAccountID,NewPoints,function(response){
+              if(response!=undefined){
+                res.send(response);
+              }else{
+                res.send({PlayerUpdatePointFailed:true});
+              }
+            });
+          }else{
+            res.send({UserAccountIDExist:false});
+          }
+        });
+        function UserAccountIDCheck(callback){
+          if(!isNullOrEmpty(UserAccountID)&&UserAccountID!=undefined){
+            isUserAccountIDExist(UserAccountID,function(response){
+              let obj = response;
+              if(!isNullOrEmpty(obj)&&obj!=undefined&&obj.length>0&&obj[0].UserAccountID==UserAccountID){
+                UserAccountIDExist = true;
+                callback(null,'1');
+              }else{
+                UserAccountIDExist = false;
+                callback(null,'1');
+              }
+            });
+          }else{
+          
+            callback(null,'1');
+          }
+        }
+        function PlayerCurrentPointsCheck(callback){
+          if(UserAccountIDExist!=undefined){
+            PlayerUserAccountID(UserAccountID,function(response){
+              let obj = response;
+              if(obj!=undefined&&obj[0].CurrentPoints!=undefined){
+                CurrentPoints = obj[0].CurrentPoints;
+                callback(null,'1');
+              }else{
+                CurrentPoints = undefined;
+                callback(null,'1');
+              }
+            });
+          }else{
+            callback(null,'1');
+          }
+        }
+      }else{
+        res.send({PointInvalidValue:true});
+      }
+    }else{
+      res.send({PointEmpty:true});
+    }
+  }else{
+    res.send({UserAccountIDEmpty:true});
+  }
+});
+app.get('/Api/v1/Player/Update/UserAccountID/:UserAccountID/Subtract/Point/:Point', function (req, res) {
+  let UserAccountID = req.params.UserAccountID;
+  let Point = req.params.Point;
+  if(!isNullOrEmpty(UserAccountID)){
+    if(!isNullOrEmpty(Point)){
+      if(validator.isInt(Point)==true){
+        let UserAccountIDExist =false;
+        let CurrentPoints = undefined;
+        async.series([UserAccountIDCheck,PlayerCurrentPointsCheck],function(error,response){
+          let NewPoints = parseInt(CurrentPoints)-parseInt(Point);
           
           if(UserAccountIDExist==true){
             res.send({NewPoints:NewPoints});
