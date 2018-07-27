b0af941bf59896b241be8c832077f1d34e7e4912
Kaivaan <cariaga.info@gmail.com>
2018-07-27 15:17:07 +0800

    asf

--- server.js
+++ server.js
@@ -4018,57 +4018,60 @@ app.get('/Api/v1/Player/Update/UserAccountID/:UserAccountID/Add/Point/:Point', f
 
   if(!isNullOrEmpty(UserAccountID)){
     if(!isNullOrEmpty(Point)){
-
-      let UserAccountIDExist =false;
-      let CurrentPoints = undefined;
-      async.series([UserAccountIDCheck,PlayerCurrentPointsCheck],function(error,response){
-        let NewPoints = CurrentPoints+Point;
-        
-        if(UserAccountIDExist==true){
-          res.send({NewPoints:NewPoints});
-          /*PlayerUpdatePoint(UserAccountID,NewPoints,function(response){
-            if(response!=undefined){
-              res.send(response);
-            }else{
-              res.send({PlayerUpdatePointFailed:true});
-            }
-          });*/
-        }else{
-          res.send({UserAccountIDExist:false});
+      if(isInt(Point)==true){
+        let UserAccountIDExist =false;
+        let CurrentPoints = undefined;
+        async.series([UserAccountIDCheck,PlayerCurrentPointsCheck],function(error,response){
+          let NewPoints = parseInt(CurrentPoints)+parseInt(Point);
+          
+          if(UserAccountIDExist==true){
+            res.send({NewPoints:NewPoints});
+            /*PlayerUpdatePoint(UserAccountID,NewPoints,function(response){
+              if(response!=undefined){
+                res.send(response);
+              }else{
+                res.send({PlayerUpdatePointFailed:true});
+              }
+            });*/
+          }else{
+            res.send({UserAccountIDExist:false});
+          }
+        });
+        function UserAccountIDCheck(callback){
+          if(!isNullOrEmpty(UserAccountID)&&UserAccountID!=undefined){
+            isUserAccountIDExist(UserAccountID,function(response){
+              let obj = response;
+              if(!isNullOrEmpty(obj)&&obj!=undefined&&obj.length>0&&obj[0].UserAccountID==UserAccountID){
+                UserAccountIDExist = true;
+                callback(null,'1');
+              }else{
+                UserAccountIDExist = false;
+                callback(null,'1');
+              }
+            });
+          }else{
+          
+            callback(null,'1');
+          }
         }
-      });
-      function UserAccountIDCheck(callback){
-        if(!isNullOrEmpty(UserAccountID)&&UserAccountID!=undefined){
-          isUserAccountIDExist(UserAccountID,function(response){
-            let obj = response;
-            if(!isNullOrEmpty(obj)&&obj!=undefined&&obj.length>0&&obj[0].UserAccountID==UserAccountID){
-              UserAccountIDExist = true;
-              callback(null,'1');
-            }else{
-              UserAccountIDExist = false;
-              callback(null,'1');
-            }
-          });
-        }else{
-        
-          callback(null,'1');
+        function PlayerCurrentPointsCheck(callback){
+          if(UserAccountIDExist!=undefined){
+            PlayerUserAccountID(UserAccountID,function(response){
+              let obj = response;
+              if(obj!=undefined&&obj[0].CurrentPoints!=undefined){
+                CurrentPoints = obj[0].CurrentPoints;
+                callback(null,'1');
+              }else{
+                CurrentPoints = undefined;
+                callback(null,'1');
+              }
+            });
+          }else{
+            callback(null,'1');
+          }
         }
-      }
-      function PlayerCurrentPointsCheck(callback){
-        if(UserAccountIDExist!=undefined){
-          PlayerUserAccountID(UserAccountID,function(response){
-            let obj = response;
-            if(obj!=undefined&&obj[0].CurrentPoints!=undefined){
-              CurrentPoints = obj[0].CurrentPoints;
-              callback(null,'1');
-            }else{
-              CurrentPoints = undefined;
-              callback(null,'1');
-            }
-          });
-        }else{
-          callback(null,'1');
-        }
+      }else{
+        res.send({});
       }
     }else{
       res.send({PointEmpty:true});
