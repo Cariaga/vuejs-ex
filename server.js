43389653cd9e4ac0e15211d2acfbc92c5fb7750d
Kaivaan <cariaga.info@gmail.com>
2018-08-01 22:36:12 +0800

    asf

--- server.js
+++ server.js
@@ -4382,7 +4382,7 @@ function GameHistory(callback){
 //---GameHistory ROUTING END
 //---UserInfo ROUTING START
 app.get('/Api/v1/UserInfo/Add/UserAccountID/:UserAccountID/Email/:Email/PhoneNumber/:PhoneNumber/TelephoneNumber/:TelephoneNumber/', function (req, res) {
-  //USAGE /Api/v1/UserInfo/Add/UserAccountID/Email/PhoneNumber/TelephoneNumber
+  //USAGE /Api/v1/UserInfo/Add/UserAccountID/6f6776bd-3fd6-4dcb-a61d-ba90b5b35dc6/Email/Cariagajkl.info@gmail.com/PhoneNumber/02121547894/TelephoneNumber/1324579/
 
   //Tests for foreignKey should result in  foreign key constraint fails Error
   // /Api/v1/UserInfo/Add/5879999/Email14535432/PhoneNumber/TelephoneNumber
