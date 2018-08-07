b5487921107eb7489d3abab4f969b6a32a173454
Kaivaan <cariaga.info@gmail.com>
2018-08-07 21:39:29 +0800

    asf

--- server.js
+++ server.js
@@ -4475,6 +4475,7 @@ function TransferHistoryAll(callback){
 app.get('/Api/v1/TransferHistory/Describe', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   Models.TransferHistory.sync({alter:true});
+  Models.TransferHistory.sync();
   Models.TransferHistory.describe().then(result=>{
     res.send([result]);
   });
