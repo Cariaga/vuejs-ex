8b85d952544fb11bc7769e12face8fa977082773
Kaivaan <cariaga.info@gmail.com>
2018-08-10 20:53:50 +0800

    asf

--- server.js
+++ server.js
@@ -92,24 +92,37 @@ const  sequelize = new Sequelize('sampledb', 'user', 'user', {
   dialect: 'mysql'
 });
 
+app.use(session({
+  secret: '2C44-4D44-WppQ38S',
+  resave: true,
+  saveUninitialized: true
+}));
 
+var auth = function(req, res, next) {
+  if (req.session && req.session.user === "amy" && req.session.admin)
+    return next();
+  else
+    return res.sendStatus(401);
+};
+// Login endpoint
+app.post('/login', function (req, res) {
+  if (!req.query.username || !req.query.password) {
+    res.send('login failed');    
+  } else if(req.body.UserName === "amy") {
+    req.session.user = "amy";
+    res.send("login success!");
+  }
+});
+// Logout endpoint
+app.get('/logout', function (req, res) {
+  req.session.destroy();
+  res.send("logout success!");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Get content endpoint
+app.get('/content', auth, function (req, res) {
+  res.send("You can only see this after you've logged in.");
+});
 
 
 /*-----------------Fail
