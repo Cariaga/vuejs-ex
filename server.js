2bf2026bad3a109b7dbc411fd6d71dc6fb84c4ee
Kaivaan <cariaga.info@gmail.com>
2018-08-10 15:22:04 +0800

    asf

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ function(username, password, done) {
 */
 passport.use(new Strategy(
   function(token, cb) {
-    db.users.findByToken(token, function(err, user) {
+
+    return cb(null, user);
+   /* db.users.findByToken(token, function(err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       return cb(null, user);
-    });
+    });*/
   }));
 
 var app = express(); // create our app w/ express
@@ -103,7 +105,7 @@ function(req, res) {
 app.post('/authenticate',
   passport.authenticate('bearer', { session: false }),
   function(req, res) {
-    res.json({ username: req.user.username, email: req.user.emails[0].value });
+    res.json({ username:"User", email: "Email" });
   });
 
 
