1a4f27874a59af4adcac7310a46e0dcfddb1b281
Kaivaan <cariaga.info@gmail.com>
2018-08-10 12:10:55 +0800

    asf

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(function (req, res, next) {
   var origin = req.headers.origin;
   res.header("Access-Control-Allow-Origin", "*");
   // Request methods you wish to allow
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 
   // Request headers you wish to allow
   res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
