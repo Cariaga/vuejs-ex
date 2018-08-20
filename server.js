80f05909dc2aa3ef12c58d211b460ceee57db0d9
Kaivaan <cariaga.info@gmail.com>
2018-08-20 21:19:30 +0800

    asf

--- server.js
+++ server.js
@@ -766,7 +766,6 @@ function isPlayerUserAccountIDExist(UserAccountID,callback){
 app.get('/Api/v1/RawQuery/:RawQuery', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   let RawQuery = req.params.RawQuery;
-  RawQuery = 'SELECT * from Players group by ShopID';
 
   const connection = mysql.createConnection({
     host     : '172.30.166.206',
