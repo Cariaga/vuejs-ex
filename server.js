8c5dcb64e811e290759849b3ea7586f57ccc955c
Kaivaan <cariaga.info@gmail.com>
2018-08-20 20:37:01 +0800

    asf

--- server.js
+++ server.js
@@ -766,7 +766,7 @@ function isPlayerUserAccountIDExist(UserAccountID,callback){
 app.get('/Api/v1/RawQuery/:RawQuery', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   let RawQuery = req.params.RawQuery;
-  RawQuery = 'Select count (id),PlayersID,UserAccountID,ShopID,count(ShopID) from Players group by (ShopID)';
+  RawQuery = 'Select count (ShopID),PlayersID,UserAccountID,ShopID,count(ShopID) from Players group by (ShopID)';
 
   const connection = mysql.createConnection({
     host     : '172.30.166.206',
