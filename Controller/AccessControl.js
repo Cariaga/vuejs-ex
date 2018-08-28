6cf8372b9f7b4b52b5deef9d9a6eb2523dbe9aa6
Kaivaan <cariaga.info@gmail.com>
2018-08-28 19:55:18 +0800

    asf

--- Controller/AccessControl.js
+++ Controller/AccessControl.js
@@ -2,9 +2,6 @@
 //--Select Start
 module.exports = function(app) {
 
-
-
-    
 app.get('/Api/v1/AccessControl/Update/AccessControlID/:AccessControlID/AccessID/:AccessID/AccessName/:AccessName/AccessTags/:AccessTags', function (req, res) {
   let AccessControlID = req.params.AccessControlID;
   let AccessID = req.params.AccessID;
@@ -34,51 +31,31 @@ app.get('/Api/v1/AccessControl/Update/AccessControlID/:AccessControlID/AccessID/
     res.send({AccessControlIDMissing:true});
   }
 });
-
-    /*app.get('/selectTest', function(req, res) {
-      console.log('got the get!');
-      res.send("Worked Select");
-    });*/
-    
-    };
-  
-//--Select End
-
-//--Update Start
-module.exports = function(app) {
-    app.get('/selectTest', function(req, res) {
-      console.log('got the update!');
-      res.send("Worked Update");
-    });
-  };
-//--Update End
-
-
-//---AccessControl ROUTING START -------------------------------migrated
 app.get('/Api/v1/AccessControl/Add/AccessID/:AccessID/AccessName/:AccessName/AccessTags/:AccessTags', function (req, res) {
   let AccessID = req.params.AccessID;
   let AccessName = req.params.AccessName;
   let AccessTags = req.params.AccessTags;
-  if(!isNullOrEmpty(AccessID)){
-    if(!isNullOrEmpty(AccessName)){
-      if(!isNullOrEmpty(AccessTags)){
-        AddAccessControl(AccessID,AccessName,AccessTags,function(response) {
-          if(response!=undefined){
-            res.send(response);
-          }else{
-            res.send({AddAccessControlFailed:true});
-          }
-        });
+    if(!isNullOrEmpty(AccessID)){
+      if(!isNullOrEmpty(AccessName)){
+        if(!isNullOrEmpty(AccessTags)){
+          AddAccessControl(AccessID,AccessName,AccessTags,function(response) {
+            if(response!=undefined){
+              res.send(response);
+            }else{
+              res.send({AddAccessControlFailed:true});
+            }
+          });
+        }else{
+          res.send({AccessTagsMissing:true});
+        }
       }else{
-        res.send({AccessTagsMissing:true});
+        res.send({AccessNameMissing:true});
       }
     }else{
-      res.send({AccessNameMissing:true});
+      res.send({AccessIDMissing:true});
     }
-  }else{
-    res.send({AccessIDMissing:true});
-  }
-});
+  });
+  };
 // mmigrated
 function AddAccessControl(AccessID,AccessName,AccessTags,callback){
   var item1 = Models.AccessControl.build({
