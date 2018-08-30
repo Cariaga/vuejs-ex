ec8af20cabc3559296895f492bc96dee198d85f8
Kaivaan <cariaga.info@gmail.com>
2018-08-30 14:58:20 +0800

    asf

--- API/SharedController/DBConnect.js
+++ API/SharedController/DBConnect.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 module.exports.DBConnect = function DBConnect(RawQuery,callback){
     const connection = mysql.createConnection({
       host: '172.30.166.206'||'localhost'||'192.168.254.106',
-      user: 'root'||'user',
-      password: ''||'user',
+      user: 'new',
+      password: 'user',
       port: process.env.OPENSHIFT_MYSQL_DB_PORT||3306,
       database: 'sampledb'
     });
@@ -12,8 +12,8 @@ module.exports.DBConnect = function DBConnect(RawQuery,callback){
     connection.query(RawQuery,
       function (err, results, fields) {
         console.log(err);
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
+        /*console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available*/
         callback(results);
       });
     connection.end();
