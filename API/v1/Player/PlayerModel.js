0f9fc7bdd556d02813eb7fda3d458951bd3f7ae4
Kaivaan <cariaga.info@gmail.com>
2018-08-31 15:53:23 +0800

    asf

--- API/v1/Player/PlayerModel.js
+++ API/v1/Player/PlayerModel.js
@@ -212,8 +212,8 @@ module.exports.PlayerUpdate = function PlayerUpdate(PlayersID, UserAccountID, Sh
     `SET @Name=${Name};` +
     `SET @Surname=${Surname};` +
     `SET @CurrentRoomName=${CurrentRoomName};` +
-    "UPDATE `sampledb`.`players`"+
-    "SET ShopID = @ShopID, ScreenName = @ScreenName, Name = @Name, Surname = @Surname, CurrentRoomName = @CurrentRoomName, CurrentPoints = @CurrentPoints "+
+    "UPDATE `sampledb`.`players` "+
+    "SET ShopID = @ShopID, ScreenName = @ScreenName, Name = @Name, Surname = @Surname, CurrentRoomName = @CurrentRoomName"+
     "WHERE PlayersID = @PlayersID and UserAccountID = @UserAccountID;";
     DBConnect.DBConnect(query, function (response) {
       if (response != undefined) {
