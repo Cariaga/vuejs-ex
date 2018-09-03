83ab17bf9db6ff3e0662dda055a83f043798918f
Kaivaan <cariaga.info@gmail.com>
2018-09-03 23:34:30 +0800

    asf

--- Cheng.js
+++ Cheng.js
@@ -1,3 +1,4 @@
+//http://localhost:8080/Api/v1/RoomConfiguration/Add/RoomID/RID6/GameType/Holdem/SmallBlind/50/BigBlind/100/Speed/50
 let RoomConfigurationModel = require('./API/v1/RoomConfiguration/RoomConfigurationModel');
 RoomConfigurationModel.AddRoomConfiguration('RID88', 'Holdem', '11', '22', 'Fast', function (response) {
   console.log("done");
