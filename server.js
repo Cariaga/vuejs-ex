7971d01c07547092c6fbe7ccc27fe54e9764b09f
Kaivaan <cariaga.info@gmail.com>
2018-09-03 20:37:56 +0800

    asf

--- server.js
+++ server.js
@@ -98,10 +98,13 @@ require('./API/v1/Verification/Verification')(app);
 require('./API/v1/WithdrawHistory/WithdrawHistory')(app);
 require('./API/v1/WithdrawHistoryList/WithdrawHistoryList')(app);
 let DBConnect = require("../vuejs-ex/API/SharedController/DBConnect");
+function test(){
+ let RegisterModel= require('./API/v1/Register/RegisterModel');
+ RegisterModel.RegisterAccount('UserAccountID'+Math.random(),'AccessID','UserName','Password','ValidKey','Email','PhoneNumber','BankName','AccountNumber','SecurityCode','Valid','Expiration',function(response){
+    console.log("OK");
+  });
+}
 
-require('./API/v1/Register/RegisterModel').RegisterAccount('UserAccountID'+Math.random(),'AccessID','UserName','Password','ValidKey','Email','PhoneNumber','BankName','AccountNumber','SecurityCode','Valid','Expiration',function(response){
-  console.log("OK");
-});
 
 //--testing for season based authentication END
 
