6b0396407443b6e5bc34aeb0bcf1fa4b05d6cb21
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-03 21:04:56 +0800

    try

--- Cheng.js
+++ Cheng.js
@@ -2,3 +2,8 @@ let RoomConfigurationModel = require('./API/v1/RoomConfiguration/RoomConfigurati
 RoomConfigurationModel.AddRoomConfiguration('RID88', 'Holdem', '11', '22', 'Fast', function (response) {
   console.log("done");
 });
+
+let DBCheck = require('./API/SharedController/DBCheck');
+DBCheck.IsRoomIDExist('RID2', function (response) {
+  console.log("done");
+});
