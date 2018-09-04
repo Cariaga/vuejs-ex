ec804355a8b095466c6eaf3e22315aea0d9778db
Kaivaan <cariaga.info@gmail.com>
2018-09-04 10:54:36 +0800

    asf

--- API/v1/Login/LoginHistoryModel.js
+++ API/v1/Login/LoginHistoryModel.js
@@ -8,11 +8,11 @@ let DBConnect = require("../../SharedController/DBConnect");
 var uuidv4 = require('uuid/v4');
 module.exports.AddLoginHistory = function(UserAccountID, IP, DeviceName, DeviceRam, DeviceCpu, DateTime,callback){
     let _UserAccountID = UserAccountID;
-let _IP = IP;
-let _DeviceName = DeviceName;
-let _DeviceRam = DeviceRam;
-let _DeviceCpu = DeviceCpu;
-let _DateTime = DateTime;
+    let _IP = IP;
+    let _DeviceName = DeviceName;
+    let _DeviceRam = DeviceRam;
+    let _DeviceCpu = DeviceCpu;
+    let _DateTime = DateTime;
     let query =
     "INSERT INTO `sampledb`.`loginhistories` (`IP`, `UserAccountID`, `DeviceName`, `DeviceRam`, `DeviceCpu`, `LoginDateTime`) "+
     "VALUES ('"+_UserAccountID+"','"+_IP+"','"+_DeviceName+"','"+_DeviceRam+"','"+_DeviceCpu+"','"+_DateTime+"');"
