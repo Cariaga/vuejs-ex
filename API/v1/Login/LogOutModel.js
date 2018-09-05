b23746be9538bbd51e104e2ebdb420756be2568d
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-05 13:31:36 +0800

    a

--- API/v1/Login/LogOutModel.js
+++ API/v1/Login/LogOutModel.js
@@ -3,25 +3,20 @@ let DBCheck = require("../../SharedController/DBCheck");
 let GlobalFunctions = require("../../SharedController/GlobalFunctions");
 var beautify = require("json-beautify");
 var uuidv4 = require('uuid/v4');
-
-module.exports.LogOutUserAccount = function(UserAccountID, OnlineStatus, callback){
+let DBConnect = require("./");
+module.exports.LogOutUserAccount = function (UserAccountID, OnlineStatus, callback) {
     let _UserAccountID = UserAccountID;
-<<<<<<< HEAD
-    let _OnlineStatus = OnlineStatus;
     let query =
-    
-    "UPDATE `sampledb`.`useraccounts` " +
-    "SET OnlineStatus = '"+_OnlineStatus+"' " +
-    "WHERE UserAccountID = '"+_UserAccountID+"' ";
+        "UPDATE `sampledb`.`useraccounts` " +
+        "SET OnlineStatus = 'Offline' " +
+        "WHERE UserAccountID = '" + _UserAccountID + "' ";
 
     DBConnect.DBConnect(query, function (response) {
         if (response != undefined) {
-          console.log(response);
-          callback(response);
+            console.log(response);
+            callback(response);
         } else {
-          callback(undefined);
+            callback(undefined);
         }
-      });
-=======
->>>>>>> 87e81be59a72fa148e99272cb39f28891b5691e5
+    });
 }
