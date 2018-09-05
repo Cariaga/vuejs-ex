3220293d18fe4deb2e885b42adb5fa7d1744e6fc
Kaivaan <cariaga.info@gmail.com>
2018-09-05 14:30:23 +0800

    asf

--- API/v1/GameLogList/GameLogListModel.js
+++ API/v1/GameLogList/GameLogListModel.js
@@ -4,14 +4,28 @@ let DBConnect = require("../../SharedController/DBConnect");
 module.exports.GameLogList = function GameLogList(limit,offset,callback) {
     let _limit = limit;
     let _offset = offset;
-    let query = "SELECT * FROM sampledb.gamelog_list limit "+_limit+" offset "+_offset;
+    if(_offset!=undefined&&_limit!=undefined){
+        let query = "SELECT * FROM sampledb.gamelog_list limit "+_limit+" offset "+_offset;
 
-    DBConnect.DBConnect(query, function (response) {
-      if (response != undefined) {
-        console.log(response);
-        callback(response);
-      } else {
-        callback(undefined);
-      }
+        DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+            console.log(response);
+            callback(response);
+        } else {
+            callback(undefined);
+        }
     });
+    }else{
+        let query = "SELECT * FROM sampledb.gamelog_list;";
+
+        DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+            console.log(response);
+            callback(response);
+        } else {
+            callback(undefined);
+        }
+    });
+    }
+    
 }
