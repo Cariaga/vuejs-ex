744de3bac2a9ec111f27d75f07affdb7b9468d43
Kaivaan <cariaga.info@gmail.com>
2018-09-05 10:02:36 +0800

    asf

--- API/v1/Login/LogOut.js
+++ API/v1/Login/LogOut.js
@@ -1,15 +1,18 @@
 let DBConnect = require("../../SharedController/DBConnect");
 let DBCheck = require("../../SharedController/DBCheck");
 let GlobalFunctions = require("../../SharedController/GlobalFunctions");
+let LogOutModel = require("./LogOutModel");
 var beautify = require("json-beautify");
 var uuidv4 = require('uuid/v4');
 module.exports = function (app) {
   app.get('/Api/v1/SignOut/UserAccountID/:UserAccountID/', function (req, res) {
     let UserAccountID = req.params.UserAccountID;
-      
+
     if (!isNullOrEmpty(UserName)) {
       let status = 200;
+      LogOutModel.LogOutM()
       res.status(status).end(http.STATUS_CODES[status]);
+      
     } else {
       res.send('no params sent');
     }
