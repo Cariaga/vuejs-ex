703f5768084da40bcde02a13afd62814e770cbba
Kaivaan <cariaga.info@gmail.com>
2018-09-05 14:56:30 +0800

    asf

--- API/v1/HandHistory/HandHistoryModel.js
+++ API/v1/HandHistory/HandHistoryModel.js
@@ -88,24 +88,5 @@ module.exports.HandHistoryUserAccountID = function HandHistoryUserAccountID(User
       callback(undefined);
     }
   });
-  /*Models.HandHistory.sync();
-  let result = Models.HandHistory.findAll({
-    where: {
-      UserAccountID: UserAccountID
-    }
-  }).then(function (result) {
-    let Data = result.map(function (item) {
-      return item;
 
-    });
-    if (Data.length > 0) {
-      callback(Data);
-    } else {
-      callback(undefined);
-    }
-
-  }).catch(function (result) { //catching any then errors
-    console.log("Error " + result);
-    callback(undefined);
-  });*/
 }
