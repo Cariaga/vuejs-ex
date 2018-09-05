10872763f9ea49700c55c90386aab2fa121e4fec
Kaivaan <cariaga.info@gmail.com>
2018-09-05 14:41:16 +0800

    GameLogList

--- API/v1/GameLogList/GameLogList.js
+++ API/v1/GameLogList/GameLogList.js
@@ -1,48 +1,40 @@
-
-
-
 var GameLogListModel = require('./GameLogListModel');
 var isNullOrEmpty = require('is-null-or-empty');
 
 module.exports = function (app) {
     app.get('/Api/v1/GameLogList/', function (req, res) {
-        let Min = req.params.Min;
-        let Max = req.params.Max;
-        if(!isNullOrEmpty(Min)){
-            if(!isNullOrEmpty(Max)){
-                GameLogListModel.GameLogList(undefined,undefined,function(response){
-                    if(response!=undefined){
-                        res.send(response);
-                    }else{
-                        res.send({});
-                    }
-                });
-            }else{
-                res.send({MaxMissing:true});
+        GameLogListModel.GameLogList(undefined, undefined, function (response) {
+            if (response != undefined) {
+                res.send(response);
+            } else {
+                res.send({});
             }
-        }else{
-            res.send({MinMissing:true});
-        }
+        });
+
     });
     app.get('/Api/v1/GameLogList/Min/:Min/Max/:Max', function (req, res) {
         let Min = req.params.Min;
         let Max = req.params.Max;
-        if(!isNullOrEmpty(Min)){
-            if(!isNullOrEmpty(Max)){
-                GameLogListModel.GameLogList(Min,Max,function(response){
-                    if(response!=undefined){
+        if (!isNullOrEmpty(Min)) {
+            if (!isNullOrEmpty(Max)) {
+                GameLogListModel.GameLogList(Min, Max, function (response) {
+                    if (response != undefined) {
                         res.send(response);
-                    }else{
+                    } else {
                         res.send({});
                     }
                 });
-            }else{
-                res.send({MaxMissing:true});
+            } else {
+                res.send({
+                    MaxMissing: true
+                });
             }
-        }else{
-            res.send({MinMissing:true});
+        } else {
+            res.send({
+                MinMissing: true
+            });
         }
 
     });
-    
+
 }
