88df2820c5f36d87e655c06b1bdf34f905ba79b0
Kaivaan <cariaga.info@gmail.com>
2018-09-05 11:54:18 +0800

    asf

--- API/v1/IPList/IPListController.js
+++ API/v1/IPList/IPListController.js
@@ -7,30 +7,29 @@ const Collection = require('linqjs');
 let DBConnect = require("../../SharedController/DBConnect");
 var uuidv4 = require('uuid/v4');
 
-module.exports.IPList = function IPList(limit,offset,callback) {
+module.exports.IPList = function IPList(limit, offset, callback) {
     let _limit = limit;
     let _offset = offset;
 
-    if(limit!=undefined&&offset!=undefined){
-        let query = "SELECT PlayerUserAccountID, RegisteredDateTime, ScreenName, PlayerCurrentPoints, ShopID, DistributorID, HeadOfficeID, IP FROM sampledb.player_iplist LIMIT '"+_limit+"' OFFSET "+_offset;
-    DBConnect.DBConnect(query, function (response) {
-      if (response != undefined) {
-        console.log(response);
-        callback(response);
-      } else {
-        callback(undefined);
-      }
-    });
-    }else if(limit==undefined&&offset==undefined){
+    if (limit != undefined && offset != undefined) {
+        let query = "SELECT PlayerUserAccountID, RegisteredDateTime, ScreenName, PlayerCurrentPoints, ShopID, DistributorID, HeadOfficeID, IP FROM sampledb.player_iplist LIMIT '" + _limit + "' OFFSET " + _offset;
+        DBConnect.DBConnect(query, function (response) {
+            if (response != undefined) {
+                console.log(response);
+                callback(response);
+            } else {
+                callback(undefined);
+            }
+        });
+    } else if (limit == undefined && offset == undefined) {
         let query = "SELECT PlayerUserAccountID, RegisteredDateTime, ScreenName, PlayerCurrentPoints, ShopID, DistributorID, HeadOfficeID, IP FROM sampledb.player_iplist";
-    DBConnect.DBConnect(query, function (response) {
-      if (response != undefined) {
-        console.log(response);
-        callback(response);
-      } else {
-        callback(undefined);
-      }
+        DBConnect.DBConnect(query, function (response) {
+            if (response != undefined) {
+                console.log(response);
+                callback(response);
+            } else {
+                callback(undefined);
+            }
+        })
     }
-    
-
 }
