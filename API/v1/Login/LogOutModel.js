8151fda20a964d6310149fd3003a68db58fcc1af
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-05 10:01:27 +0800

    k

---
+++ API/v1/Login/LogOutModel.js
@@ -0,0 +1,10 @@
+let DBConnect = require("../../SharedController/DBConnect");
+let DBCheck = require("../../SharedController/DBCheck");
+let GlobalFunctions = require("../../SharedController/GlobalFunctions");
+var beautify = require("json-beautify");
+var uuidv4 = require('uuid/v4');
+
+module.exports.LogOutUserAccount = function(UserAccountID, callback){
+    let _UserAccountID = UserAccountID;
+    
+}
