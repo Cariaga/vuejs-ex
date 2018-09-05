4c17a96cab01c0d3c8c792d427d6b4ca393dfc41
Kaivaan <cariaga.info@gmail.com>
2018-09-05 11:52:52 +0800

    asf

--- API/v1/IPList/IPListController.js
+++ API/v1/IPList/IPListController.js
@@ -7,8 +7,12 @@ const Collection = require('linqjs');
 let DBConnect = require("../../SharedController/DBConnect");
 var uuidv4 = require('uuid/v4');
 
-module.exports.IPList = function IPList(callback) {
-    let query = '';
+module.exports.IPList = function IPList(limit,offset,callback) {
+    let _limit = limit;
+    let _offset = offset;
+
+    if(limit!=undefined&&offset!=undefined){
+        let query = "SELECT PlayerUserAccountID, RegisteredDateTime, ScreenName, PlayerCurrentPoints, ShopID, DistributorID, HeadOfficeID, IP FROM sampledb.player_iplist LIMIT '"+_limit+"' OFFSET "+_offset;
     DBConnect.DBConnect(query, function (response) {
       if (response != undefined) {
         console.log(response);
@@ -17,4 +21,16 @@ module.exports.IPList = function IPList(callback) {
         callback(undefined);
       }
     });
+    }else if(limit==undefined&&offset==undefined){
+        let query = "SELECT PlayerUserAccountID, RegisteredDateTime, ScreenName, PlayerCurrentPoints, ShopID, DistributorID, HeadOfficeID, IP FROM sampledb.player_iplist";
+    DBConnect.DBConnect(query, function (response) {
+      if (response != undefined) {
+        console.log(response);
+        callback(response);
+      } else {
+        callback(undefined);
+      }
+    }
+    
+
 }
