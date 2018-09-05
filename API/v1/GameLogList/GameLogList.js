96178980e738389d0193bd0a4402156c2d4c7c6a
Kaivaan <cariaga.info@gmail.com>
2018-09-05 14:27:14 +0800

    asf

--- API/v1/GameLogList/GameLogList.js
+++ API/v1/GameLogList/GameLogList.js
@@ -5,6 +5,25 @@ var GameLogListModel = require('./GameLogListModel');
 var isNullOrEmpty = require('is-null-or-empty');
 
 module.exports = function (app) {
+    app.get('/Api/v1/GameLogList/', function (req, res) {
+        let Min = req.params.Min;
+        let Max = req.params.Max;
+        if(!isNullOrEmpty(Min)){
+            if(!isNullOrEmpty(Max)){
+                GameLogListModel.GameLogList(undefined,undefined,function(response){
+                    if(response!=undefined){
+                        res.send(response);
+                    }else{
+                        res.send({});
+                    }
+                });
+            }else{
+                res.send({MaxMissing:true});
+            }
+        }else{
+            res.send({MinMissing:true});
+        }
+    });
     app.get('/Api/v1/GameLogList/Min/:Min/Max/:Max', function (req, res) {
         let Min = req.params.Min;
         let Max = req.params.Max;
@@ -25,23 +44,5 @@ module.exports = function (app) {
         }
 
     });
-    app.get('/Api/v1/GameLogList/', function (req, res) {
-        let Min = req.params.Min;
-        let Max = req.params.Max;
-        if(!isNullOrEmpty(Min)){
-            if(!isNullOrEmpty(Max)){
-                GameLogListModel.GameLogList(undefined,undefined,function(response){
-                    if(response!=undefined){
-                        res.send(response);
-                    }else{
-                        res.send({});
-                    }
-                });
-            }else{
-                res.send({MaxMissing:true});
-            }
-        }else{
-            res.send({MinMissing:true});
-        }
-    });
+    
 }
--- API/v1/GameLogList/GameLogListModel.js
+++ API/v1/GameLogList/GameLogListModel.js
@@ -1,9 +1,11 @@
 
 let DBConnect = require("../../SharedController/DBConnect");
 
-module.exports.GameLogList = function GameLogList(callback) {
-    let query = '';
-    
+module.exports.GameLogList = function GameLogList(limit,offset,callback) {
+    let _limit = limit;
+    let _offset = offset;
+    let query = "SELECT * FROM sampledb.gamelog_list limit "+_limit+" offset "+_offset;
+
     DBConnect.DBConnect(query, function (response) {
       if (response != undefined) {
         console.log(response);
