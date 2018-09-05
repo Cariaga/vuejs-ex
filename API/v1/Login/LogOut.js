3bfabbb4c67bf4390544129bf199d029b38b9b1b
Kaivaan <cariaga.info@gmail.com>
2018-09-05 09:59:35 +0800

    asf

--- API/v1/Login/LogOut.js
+++ API/v1/Login/LogOut.js
@@ -4,14 +4,12 @@ let GlobalFunctions = require("../../SharedController/GlobalFunctions");
 var beautify = require("json-beautify");
 var uuidv4 = require('uuid/v4');
 module.exports = function (app) {
-  app.get('/Api/v1/SignOut/:UserName/:SignOutKey', function (req, res) {
-    let UserName = req.params.UserName;
-    let Password = req.params.SignOutKey;
-
-    if (!isNullOrEmpty(UserName) &&
-      !isNullOrEmpty(SignOutKey)) {
-
-      res.send('test login');
+  app.get('/Api/v1/SignOut/UserAccountID/:UserAccountID/', function (req, res) {
+    let UserAccountID = req.params.UserAccountID;
+      
+    if (!isNullOrEmpty(UserName)) {
+      let status = 200;
+      res.status(status).end(http.STATUS_CODES[status]);
     } else {
       res.send('no params sent');
     }
