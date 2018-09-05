43ca3a6927989d7f658d2ae0ebedae921fbaad65
Kaivaan <cariaga.info@gmail.com>
2018-09-05 14:16:41 +0800

    asf

--- API/v1/GameLogList/GameLogList.js
+++ API/v1/GameLogList/GameLogList.js
@@ -26,6 +26,22 @@ module.exports = function (app) {
 
     });
     app.get('/Api/v1/GameLogList/', function (req, res) {
-        res.send("asf");
+        let Min = req.params.Min;
+        let Max = req.params.Max;
+        if(!isNullOrEmpty(Min)){
+            if(!isNullOrEmpty(Max)){
+                GameLogListModel.GameLogList(undefined,undefined,function(response){
+                    if(response!=undefined){
+                        res.send(response);
+                    }else{
+                        res.send({});
+                    }
+                });
+            }else{
+                res.send({MaxMissing:true});
+            }
+        }else{
+            res.send({MinMissing:true});
+        }
     });
 }
