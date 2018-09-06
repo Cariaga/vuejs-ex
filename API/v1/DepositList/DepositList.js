3616833aa61c03e046e034113448478f3cf29376
Kaivaan <cariaga.info@gmail.com>
2018-09-06 17:37:31 +0800

    asf

--- API/v1/DepositList/DepositList.js
+++ API/v1/DepositList/DepositList.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
       res.send(response);
     });
   });
-  app.get('/Api/v1/DepositList/limit/:limit/offset/:offset', function (req, res) {
+  app.get('/Api/v1/DepositList/limit/:limit/offset/:offset/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     let limit = req.params.limit;
     let offset = req.params.offset;
--- API/v1/DepositList/DepositListModel.js
+++ API/v1/DepositList/DepositListModel.js
@@ -11,7 +11,7 @@ let DBConnect = require("../../SharedController/DBConnect");
 module.exports.DepositList = function DepositList(limit,Offset,callback) {
     let _limit = limit;
     let _Offset = Offset;
-    let query = "select * from deposit_list limit "+_Min+" Offset "+_Offset;
+    let query = "select * from deposit_list limit "+_limit+" Offset "+_Offset;
     DBConnect.DBConnect(query,function(response){
       if(response!=undefined){
         console.log(response);
