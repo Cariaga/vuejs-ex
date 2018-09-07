9da97e3ac4e8dcffb641f06917ac8faadc556368
Kaivaan <cariaga.info@gmail.com>
2018-09-07 12:14:19 +0800

    asf

--- API/v1/AccountVerification/AccountVerification.js
+++ API/v1/AccountVerification/AccountVerification.js
@@ -1,7 +1,7 @@
 var AccountVerificationModel = require('./AccountVerificationModel');
 module.exports = function (app) {
       //MODIFY
-  app.get('/Api/v1/UserAccount/Update/UserAccountID/:UserAccountID/Key/:Key', function (req, res) { 
+  app.get('/Api/v1/AccountVerification/Update/UserAccountID/:UserAccountID/Key/:Key', function (req, res) { 
     let UserAccountID = req.params.UserAccountID;
     let Key = req.params.Key;
     AccountVerificationModel.VerifyAccountUserAccountID(UserAccountID, function (response) {
