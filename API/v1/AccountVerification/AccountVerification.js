cfeb6d151a63e2c8b030e06bb917f1803a3b4477
Kaivaan <cariaga.info@gmail.com>
2018-09-07 12:13:40 +0800

    asf

--- API/v1/AccountVerification/AccountVerification.js
+++ API/v1/AccountVerification/AccountVerification.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.get('/Api/v1/UserAccount/Update/UserAccountID/:UserAccountID/Key/:Key', function (req, res) { 
     let UserAccountID = req.params.UserAccountID;
     let Key = req.params.Key;
-    AccountVerificationModel.VerifyAccountUserAccountID(UserAccountID, Verify, function (response) {
-       
+    AccountVerificationModel.VerifyAccountUserAccountID(UserAccountID, function (response) {
+        res.send(response);
       });
   });
 }
--- API/v1/AccountVerification/AccountVerificationModel.js
+++ API/v1/AccountVerification/AccountVerificationModel.js
@@ -3,14 +3,12 @@
  *
  *
  * @param {*} UserAccountID
- * @param {*} VerifiedStatus
  * @param {*} callback
  */
-module.exports.VerifyAccountUserAccountID = function VerifyAccountUserAccountID(UserAccountID, VerifiedStatus, callback) { // Verification With UserAccountID // Forcing Account To be Verified // Via UserAccountID
-    let query = 
-    `SET @UserAccountID=${UserAccountID};`+
-    `SET @VerifiedStatus=${VerifiedStatus};`+
-  
+module.exports.VerifyAccountUserAccountID = function VerifyAccountUserAccountID(UserAccountID, callback) { // Verification With UserAccountID
+    let _UserAccountID = UserAccountID;
+    let query =  
+    "UPDATE `sampledb`.`useraccounts` SET `Verified` = 'true' WHERE (`UserAccountID` = '"+_UserAccountID+"');";
     DBConnect.DBConnect(query,function(response){
       if(response!=undefined){
         console.log(response);
@@ -19,19 +17,4 @@ module.exports.VerifyAccountUserAccountID = function VerifyAccountUserAccountID(
         callback(undefined);
       }
     });
-    /*Models.UserAccount.update({
-        Verify: VerifiedStatus
-      }, {
-        where: {
-          UserAccountID: UserAccountID
-        }
-      })
-      .then(Success => {
-        callback("Updated");
-      })
-  
-      .catch(error => {
-        console.log("Error Updating " + error);
-        callback(undefined);
-      });*/
   }
