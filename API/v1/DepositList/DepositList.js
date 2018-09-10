2e1ee3a300228b273a20e3ba1021df2b0c8ee0b2
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-10 16:06:20 +0800

    up

--- API/v1/DepositList/DepositList.js
+++ API/v1/DepositList/DepositList.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
       res.send(response);
     });
   });
-  app.get('/Api/v1/DepositList/limit/:limit/offset/:offset/', function (req, res) {
+  app.get('/Api/v1/DepositList/Limit/:Limit/Offset/:Offset/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     let limit = req.params.limit;
     let offset = req.params.offset;
@@ -20,4 +20,28 @@ module.exports = function (app) {
     });
   });
 
+  app.get('/Api/v1/DepositList/Search/Column/:Column/Value/:Value', function (req, res) {
+    let Column = req.params.Column;
+    let Value = req.params.Value;
+
+    if (!isNullOrEmpty(Column)) {
+      if (!isNullOrEmpty(Value)) {
+        DepositListModel.DepositSearch(Column, Value, function (response) {
+          if (response != undefined) {
+            res.send(response);
+          } else {
+            res.send(undefined);
+          }
+        });
+      } else {
+        res.send({
+          InvalidValue: true
+        });
+      }
+    } else {
+      res.send({
+        InvalidColumn: true
+      });
+    }
+  });
 }
