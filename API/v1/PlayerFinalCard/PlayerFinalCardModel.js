f34f06256c5619be2359662418f0f0c0e7b5129f
Kaivaan <cariaga.info@gmail.com>
2018-09-10 17:05:28 +0800

    asf

--- API/v1/PlayerFinalCard/PlayerFinalCardModel.js
+++ API/v1/PlayerFinalCard/PlayerFinalCardModel.js
@@ -5,3 +5,8 @@ var async = require("async");
 var moment = require('moment');
 const Collection = require('linqjs');
 let DBConnect = require("../../SharedController/DBConnect");
+
+module.exports.RegisterAccount = function RegisterAccount(UserAccountID, SeasonID, Rank, Score, Card, DateTime, BeforePoints, AfterPoints, WinPoints, callback) {
+    let query ="INSERT INTO `sampledb`.`playerfinalcard` (`UserAccountID`, `SeasonID`, `Rank`, `Score`, `Card`, `DateTime`, `BeforePoints`, `AfterPoints`, `WinPoints`) "+
+    "VALUES ('Account8', 'S2', 'RK2', '0', 'C3', '1996-05-31 00:00:00', '200', '0', '0');"
+}
