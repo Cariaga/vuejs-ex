cbb5a7db26b1fe0eca18b264540fd9c7fbd9915c
Kaivaan <cariaga.info@gmail.com>
2018-09-10 20:24:28 +0800

    asf

--- API/v1/MemberList/MemberListModel.js
+++ API/v1/MemberList/MemberListModel.js
@@ -7,3 +7,31 @@ const Collection = require('linqjs');
 let DBConnect = require("../../SharedController/DBConnect");
 var uuidv4 = require('uuid/v4');
 
+module.exports.MemberListSearch = function MemberListSearch(Column, Value, callback) {
+    let _Column = Column;
+    let _Value = Value;
+    let query = 
+    "SELECT * FROM sampledb.member_list where "+_Column+" like '%"+_Value+"%';";
+    DBConnect.DBConnect(query, function (response) {
+      if (response != undefined) {
+        console.log(response);
+        callback(response);
+      } else {
+        callback(undefined);
+      }
+    });
+  }
+module.exports.MemberListSearch = function MemberListSearch(Limit, Offset, callback) {
+    let _Limit = Limit;
+    let _Offset = Offset;
+    let query = 
+    "SELECT * FROM sampledb.member_list";
+    DBConnect.DBConnect(query, function (response) {
+      if (response != undefined) {
+        console.log(response);
+        callback(response);
+      } else {
+        callback(undefined);
+      }
+    });
+  }
