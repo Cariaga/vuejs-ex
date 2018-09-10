f511a1dbd3ba55afea631ca622e3320c39902bee
Kaivaan <cariaga.info@gmail.com>
2018-09-10 17:11:45 +0800

    asf

--- API/v1/PlayerFinalCard/PlayerFinalCardModel.js
+++ API/v1/PlayerFinalCard/PlayerFinalCardModel.js
@@ -6,7 +6,25 @@ var moment = require('moment');
 const Collection = require('linqjs');
 let DBConnect = require("../../SharedController/DBConnect");
 
-module.exports.RegisterAccount = function RegisterAccount(UserAccountID, SeasonID, Rank, Score, Card, DateTime, BeforePoints, AfterPoints, WinPoints, callback) {
+module.exports.AddPlayerFinalCard = function AddPlayerFinalCard(UserAccountID, SeasonID, Rank, Score, Card, DateTime, BeforePoints, AfterPoints, WinPoints, callback) {
+    let _UserAccountID = UserAccountID;
+    let _SeasonID = SeasonID;
+    let _Rank = Rank;
+    let _Score = Score;
+    let _Card = Card;
+    let _DateTime = DateTime;
+    let _BeforePoints = BeforePoints;
+    let _AfterPoints = AfterPoints;
+    let _WinPoints = WinPoints;
     let query ="INSERT INTO `sampledb`.`playerfinalcard` (`UserAccountID`, `SeasonID`, `Rank`, `Score`, `Card`, `DateTime`, `BeforePoints`, `AfterPoints`, `WinPoints`) "+
-    "VALUES ('Account8', 'S2', 'RK2', '0', 'C3', '1996-05-31 00:00:00', '200', '0', '0');"
+    "VALUES ('"+_UserAccountID+"', '"+_SeasonID+"', '"+_Rank+"', '"+_Score+"', '"+_Card+"', '"+_DateTime+"', '"+_BeforePoints+"', '"+_AfterPoints+"', '"+_WinPoints+"');"
+    DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+          console.log(response);
+          callback(response);
+        } else {
+          callback(undefined);
+        }
+      });
+
 }
