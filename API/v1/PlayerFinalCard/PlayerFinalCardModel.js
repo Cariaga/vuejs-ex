06da4284140a7820646b5bf184422b135b2ed8a5
Kaivaan <cariaga.info@gmail.com>
2018-09-10 17:51:10 +0800

    asf

--- API/v1/PlayerFinalCard/PlayerFinalCardModel.js
+++ API/v1/PlayerFinalCard/PlayerFinalCardModel.js
@@ -27,3 +27,20 @@ module.exports.AddPlayerFinalCard = function AddPlayerFinalCard(UserAccountID, S
         }
       });
 }
+module.exports.UpdatePlayerFinalCard = function UpdatePlayerFinalCard(UserAccountID, SeasonID, DateTime, BeforePoints, AfterPoints, WinPoints, callback) {
+    let _UserAccountID = UserAccountID;
+    let _SeasonID = SeasonID;
+    let _DateTime = DateTime;
+    let _BeforePoints = BeforePoints;
+    let _AfterPoints = AfterPoints;
+    let _WinPoints = WinPoints;
+    let query ="";
+    DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+          console.log(response);
+          callback(response);
+        } else {
+          callback(undefined);
+        }
+      });
+}
