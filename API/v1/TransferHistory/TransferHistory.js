26aa01e7e1b17e9c63350d3b8cf3691e9fa58ac9
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-10 18:49:33 +0800

    up

--- API/v1/TransferHistory/TransferHistory.js
+++ API/v1/TransferHistory/TransferHistory.js
@@ -363,4 +363,29 @@ module.exports = function (app) { //MODIFY
        res.send(beautify(result, null, 2, 100));
      });
    });*/
+
+   app.get('/Api/v1/TransferHistory/Search/Column/:Column/Value/:Value', function (req, res) {
+    let Column = req.params.Column;
+    let Value = req.params.Value;
+
+    if (!isNullOrEmpty(Column)) {
+      if (!isNullOrEmpty(Value)) {
+        TransferHistoryModel.TransferHistorySearch(Column, Value, function (response) {
+          if (response != undefined) {
+            res.send(response);
+          } else {
+            res.send(undefined);
+          }
+        });
+      } else {
+        res.send({
+          InvalidValue: true
+        });
+      }
+    } else {
+      res.send({
+        InvalidColumn: true
+      });
+    }
+  });
 }
