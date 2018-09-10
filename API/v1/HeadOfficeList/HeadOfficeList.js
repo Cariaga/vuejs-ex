a9efcb233f0d39030ad593bfdb705d9624083964
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-10 23:01:23 +0800

    up

--- API/v1/HeadOfficeList/HeadOfficeList.js
+++ API/v1/HeadOfficeList/HeadOfficeList.js
@@ -5,52 +5,53 @@ let HeadOfficeListModel = require("../HeadOfficeList/HeadOfficeListModel");
 var beautify = require("json-beautify");
 var isNullOrEmpty = require('is-null-or-empty');
 
-app.get('/Api/v1/HeadOfficeList/Search/Column/:Column/Value/:Value', function (req, res) {
-    let Column = req.params.Column;
-    let Value = req.params.Value;
+module.exports = function (app) {
+    app.get('/Api/v1/HeadOfficeList/Search/Column/:Column/Value/:Value', function (req, res) {
+        let Column = req.params.Column;
+        let Value = req.params.Value;
 
-    if (!isNullOrEmpty(Column)) {
-        if (!isNullOrEmpty(Value)) {
-            HeadOfficeListModel.HeadOfficeListSearch(Column, Value, function (response) {
-                if (response != undefined) {
-                    res.send(response);
-                } else {
-                    res.send(undefined);
-                }
-            });
+        if (!isNullOrEmpty(Column)) {
+            if (!isNullOrEmpty(Value)) {
+                HeadOfficeListModel.HeadOfficeListSearch(Column, Value, function (response) {
+                    if (response != undefined) {
+                        res.send(response);
+                    } else {
+                        res.send(undefined);
+                    }
+                });
+            } else {
+                res.send({
+                    InvalidValue: true
+                });
+            }
         } else {
             res.send({
-                InvalidValue: true
+                InvalidColumn: true
             });
         }
-    } else {
-        res.send({
-            InvalidColumn: true
-        });
-    }
-});
+    });
 
-app.get('/Api/v1/HeadOfficeList/Limit/:Limit/Offset/:Offset', function (req, res) {
-    let Limit = req.params.Limit;
-    let Offset = req.params.Offset;
-    if (!isNullOrEmpty(Limit)) {
-        if (!isNullOrEmpty(Offset)) {
-            HeadOfficeListModel.HeadOfficeList(Limit, Offset, function (response) {
-                if (response != undefined) {
-                    res.send(response);
-                } else {
-                    res.send({});
-                }
-            });
+    app.get('/Api/v1/HeadOfficeList/Limit/:Limit/Offset/:Offset', function (req, res) {
+        let Limit = req.params.Limit;
+        let Offset = req.params.Offset;
+        if (!isNullOrEmpty(Limit)) {
+            if (!isNullOrEmpty(Offset)) {
+                HeadOfficeListModel.HeadOfficeList(Limit, Offset, function (response) {
+                    if (response != undefined) {
+                        res.send(response);
+                    } else {
+                        res.send({});
+                    }
+                });
+            } else {
+                res.send({
+                    OffsetMissing: true
+                });
+            }
         } else {
             res.send({
-                OffsetMissing: true
+                LimitMissing: true
             });
         }
-    } else {
-        res.send({
-            LimitMissing: true
-        });
-    }
-
-});
+    });
+}
