0bf7d9ed085884e69a9c82b8199a4552aedac7a9
Kaivaan <cariaga.info@gmail.com>
2018-09-10 22:36:55 +0800

    asf

---
+++ API/v1/HeadOfficeList/HeadOfficeList.js
@@ -0,0 +1,6 @@
+let DBConnect = require("../../SharedController/DBConnect");
+let DBCheck = require("../../SharedController/DBCheck");
+let GlobalFunctions = require("../../SharedController/GlobalFunctions");
+let HeadOfficeModel = require("../HeadOffice/HeadOfficeModel");
+var beautify = require("json-beautify");
+var isNullOrEmpty = require('is-null-or-empty');
