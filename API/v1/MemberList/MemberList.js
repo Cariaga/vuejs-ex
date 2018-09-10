d9af74bd48c85cc78998e3fc69568d6c47c6e690
Kaivaan <cariaga.info@gmail.com>
2018-09-10 22:45:07 +0800

    asf

--- API/v1/MemberList/MemberList.js
+++ API/v1/MemberList/MemberList.js
@@ -7,7 +7,13 @@ var MemberListModel = require('../MemberList/MemberListModel')
 module.exports = function (app) { //SELECTION
 
   app.get('/Api/v1/MembersList/Limit/:Limit/Offset/:Offset', function (req, res) {
-
+    let Limit = req.params.Limit;
+    let Offset = req.params.Offset;
+    if(!isNullOrEmpty(Limit)){
+      if(!isNullOrEmpty(Offset)){
+        
+      }
+    }
   });
 
   app.get('/Api/v1/MemberList/Search/Column/:Column/Value/:Value', function (req, res) {
