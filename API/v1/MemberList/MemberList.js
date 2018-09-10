a52e30af91e50f4b425e7e82760e5e0cd9988655
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-10 20:42:49 +0800

    up

--- API/v1/MemberList/MemberList.js
+++ API/v1/MemberList/MemberList.js
@@ -3,6 +3,7 @@ let DBCheck = require("../../SharedController/DBCheck");
 let GlobalFunctions = require("../../SharedController/GlobalFunctions");
 var beautify = require("json-beautify");
 var isNullOrEmpty = require('is-null-or-empty');
+var MemberListModel = require('../MemberList/MemberListModel')
 module.exports = function (app) { //SELECTION
 
   app.get('/Api/v1/MembersList/Limit/:Limit/Offset/:Offset', function (req, res) {
@@ -140,4 +141,29 @@ module.exports = function (app) { //SELECTION
       });
     }
   });*/
+
+  app.get('/Api/v1/MemberList/Search/Column/:Column/Value/:Value', function (req, res) {
+    let Column = req.params.Column;
+    let Value = req.params.Value;
+
+    if (!isNullOrEmpty(Column)) {
+      if (!isNullOrEmpty(Value)) {
+        MemberListModel.MemberListSearch(Column, Value, function (response) {
+          if (response != undefined) {
+            res.send(response);
+          } else {
+            res.send(undefined);
+          }
+        });
+      } else {
+        res.send({
+          InvalidValue: true
+        });
+      }
+    } else {
+      res.send({
+        InvalidColumn: true
+      });
+    }
+  });
 }
