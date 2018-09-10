20f0d118435279752fc81023baf76d821afb9079
theklayd <clydesantos1028@gmail.com>
2018-09-10 16:07:21 +0800

    wwww

--- API/v1/DepositList/DepositList.js
+++ API/v1/DepositList/DepositList.js
@@ -5,12 +5,14 @@ let DepositListModel = require("../DepositList/DepositListModel");
 var beautify = require("json-beautify");
 var isNullOrEmpty = require('is-null-or-empty');
 module.exports = function (app) {
+
   app.get('/Api/v1/DepositList/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     DepositListModel.DepositList(undefined,undefined,function(response){
       res.send(response);
     });
   });
+  
   app.get('/Api/v1/DepositList/limit/:limit/offset/:offset/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     let limit = req.params.limit;
