1eb8860c8f7af847e8123be5ba690f975c0b63d2
Kaivaan <cariaga.info@gmail.com>
2018-09-10 23:20:34 +0800

    removed

--- API/v1/SupportTicket/SupportTicket.js
+++ API/v1/SupportTicket/SupportTicket.js
@@ -189,55 +189,6 @@ module.exports = function (app) { //INSERT
       });
     }
   });
-  //SELECTION
-  app.get('/Api/v1/SupportTicket/', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    let Offset = req.query.Offset;
-    let Limit = req.query.Limit;
-    let Sort = req.query.Sort;
-    Models.SupportTicket.sync( /*{alter:true}*/ ); //Never call Alter and force during a sequelize.query alter table without matching the model with the database first if you do records will be nulled alter is only safe when it matches the database
-    if (isNullOrEmpty(Offset) && isNullOrEmpty(Limit) && isNullOrEmpty(Sort)) {
-      Models.SupportTicket.sync();
-      let result = Models.SupportTicket.findAll({
-        where: {
-          SupportTicketID: {
-            ne: null //not null
-          }
-        }
-      }).then(function (result) {
-        let Data = result.map(function (item) {
-          return item;
-
-        });
-
-        res.send(beautify(Data, null, 2, 100));
-      }).catch(function (result) { //catching any then errors
-
-        res.send("Error " + result);
-      });
-    }
-    if (!isNullOrEmpty(Offset) && !isNullOrEmpty(Limit) && !isNullOrEmpty(Sort)) {
-
-    }
-    if (!isNullOrEmpty(Offset) && !isNullOrEmpty(Limit) && isNullOrEmpty(Sort)) {
-
-    }
-    if (!isNullOrEmpty(Offset) && isNullOrEmpty(Limit) && !isNullOrEmpty(Sort)) {
-
-    }
-    if (isNullOrEmpty(Offset) && !isNullOrEmpty(Limit) && !isNullOrEmpty(Sort)) {
-
-    }
-    if (isNullOrEmpty(Offset) && isNullOrEmpty(Limit) && !isNullOrEmpty(Sort)) {
-
-    }
-    if (!isNullOrEmpty(Offset) && isNullOrEmpty(Limit) && isNullOrEmpty(Sort)) {
-
-    }
-    //res.send("SupportTicket "+Offset+" "+ Limit+" "+Sort);
-  });
-
-
   app.get('/Api/v1/SupportTicket/UserAccountID/:UserAccountID/Status/:Status', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     let UserAccountID = req.params.UserAccountID;
@@ -263,36 +214,5 @@ module.exports = function (app) { //INSERT
         InvalidUserAccountID: true
       });
     }
-  });
-  //STRUCTURE
-  app.get('/Api/v1/SupportTicket/Describe', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    Models.SupportTicket.sync( /*{alter:true}*/ ); //Never call Alter and force during a sequelize.query alter table without matching the model with the database first if you do records will be nulled alter is only safe when it matches the database
-    Models.SupportTicket.describe().then(result => {
-      res.send(beautify(result, null, 2, 100));
-    });
-  });
-
-  app.get('/Api/v1/SupportTicket/Clear', function (req, res) {
-    Models.SupportTicket.destroy({
-        where: {},
-        truncate: true
-      })
-      .then(Success => {
-        res.send("Cleared");
-      })
-      .catch(err => {
-        res.send("Truncate " + err);
-      });
-  });
-  app.get('/Api/v1/SupportTicket/Delete', function (req, res) {
-    Models.SupportTicket.sync({
-      force: true
-    }).then(function (result) {
-      res.send("Deleted");
-    }).catch(function (result) { //catching any then errors
-
-      res.send("Error " + result);
-    });
   });
   }
