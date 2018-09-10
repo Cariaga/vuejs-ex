705c95365fa34631a894218081971b0e450e6602
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-10 22:51:58 +0800

    up

--- API/v1/HeadOfficeList/HeadOfficeList.js
+++ API/v1/HeadOfficeList/HeadOfficeList.js
@@ -1,6 +1,31 @@
 let DBConnect = require("../../SharedController/DBConnect");
 let DBCheck = require("../../SharedController/DBCheck");
 let GlobalFunctions = require("../../SharedController/GlobalFunctions");
-let HeadOfficeModel = require("../HeadOffice/HeadOfficeModel");
+let HeadOfficeListModel = require("../HeadOfficeList/HeadOfficeListModel");
 var beautify = require("json-beautify");
 var isNullOrEmpty = require('is-null-or-empty');
+
+app.get('/Api/v1/HeadOfficeList/Search/Column/:Column/Value/:Value', function (req, res) {
+    let Column = req.params.Column;
+    let Value = req.params.Value;
+
+    if (!isNullOrEmpty(Column)) {
+        if (!isNullOrEmpty(Value)) {
+            HeadOfficeListModel.HeadOfficeListSearch(Column, Value, function (response) {
+                if (response != undefined) {
+                    res.send(response);
+                } else {
+                    res.send(undefined);
+                }
+            });
+        } else {
+            res.send({
+                InvalidValue: true
+            });
+        }
+    } else {
+        res.send({
+            InvalidColumn: true
+        });
+    }
+});
