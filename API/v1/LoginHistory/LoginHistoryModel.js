13b6965f81b6dc01ddf2d52d34318488e34d361f
Kaivaan <cariaga.info@gmail.com>
2018-09-11 13:40:53 +0800

    asf

--- API/v1/LoginHistory/LoginHistoryModel.js
+++ API/v1/LoginHistory/LoginHistoryModel.js
@@ -9,42 +9,6 @@ var uuidv4 = require('uuid/v4');
 /**
  *
  *
- * @param {*} LoginHistoryID
- * @param {*} UserAccountID
- * @param {*} IP
- * @param {*} DeviceName
- * @param {*} DeviceRam
- * @param {*} DeviceCpu
- * @param {*} Time
- * @param {*} Date
- * @param {*} callback
- */
-/*
-module.exports.LoginHistoryUpdate = function LoginHistoryUpdate(LoginHistoryID, UserAccountID, IP, DeviceName, DeviceRam, DeviceCpu, LoginDateTime, callback) {
-  let query =
-    `SET @LoginHistoryID=${LoginHistoryID};` +
-    `SET @UserAccountID=${UserAccountID};` +
-    `SET @IP=${IP};` +
-    `SET @DeviceName=${DeviceName};` +
-    `SET @DeviceRam=${DeviceRam};` +
-    `SET @DeviceCpu=${DeviceCpu};` +
-    `SET @LoginDateTime=${LoginDateTime};` +
-    "UPDATE `sampledb`.`loginhistories`"+
-    "SET IP = @IP, UserAccountID = @UserAccountID, DeviceName = @DeviceName, DeviceRam = @DeviceRam, DeviceCpu = @DeviceCpu, LoginDateTime = @LoginDateTime "+
-    "WHERE LoginHistoryID = @LoginHistoryID;";
-    DBConnect.DBConnect(query, function (response) {
-      if (response != undefined) {
-        console.log(response);
-        callback(response);
-      } else {
-        callback(undefined);
-      }
-    });
-}*/
-
-/**
- *
- *
  * @param {*} UserAccountID
  * @param {*} IP
  * @param {*} DeviceName
@@ -72,24 +36,4 @@ module.exports.AddLoginHistory = function AddLoginHistory(UserAccountID, IP, Dev
         callback(undefined);
       }
     });
-  /*var item1 = Models.LoginHistory.build({
-    UserAccountID: UserAccountID,
-    IP: IP,
-    DeviceName: DeviceName,
-    DeviceRam: DeviceRam,
-    DeviceCpu: DeviceCpu,
-    Time: Time,
-    Date: Date
-  });
-  Models.LoginHistory.sync({
-    alter: true
-  }); //force recreates deletes old table
-  item1.save()
-    .then(Success => {
-      callback("Inserted");
-    })
-    .catch(error => {
-      console.log("error inserting " + error);
-      callback(undefined);
-    });*/
 }
