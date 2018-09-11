bb66617e82369712a55a68bfd09e040d89ac26f5
Kaivaan <cariaga.info@gmail.com>
2018-09-11 13:37:20 +0800

    asf

--- API/v1/GameHistory/GameHistoryModel.js
+++ API/v1/GameHistory/GameHistoryModel.js
@@ -36,30 +36,6 @@ module.exports.AddGameHistory = function AddGameHistory( RoundID, SeasonID, call
         callback(undefined);
       }
     });
-  /*Models.GameHistory.sync();
-  var item1 = Models.GameHistory.build({
-    UserAccountID: UserAccountID,
-    RoundID: RoundID,
-    SeasonID: SeasonID,
-    Rank: Rank,
-    Score: Score,
-    Card: Card,
-    Time: Time,
-    Date: Date,
-    BeforePoints: BeforePoints,
-    AfterPoints: AfterPoints
-  });
-  Models.GameHistory.sync(); //use force to delete old table non production
-  item1.save()
-    .then(Success => {
-      callback("Inserted");
-    })
-
-    .catch(error => {
-
-      console.log("error inserting " + error);
-      callback(undefined);
-    });*/
 }
 /**
  *
@@ -76,26 +52,4 @@ module.exports.GameHistory = function GameHistory(callback) {
       callback(undefined);
     }
   });
-  /*Models.GameHistory.sync();
-  let result = Models.GameHistory.findAll({
-    where: {
-      GameHistoryID: {
-        ne: null //not null
-      }
-    }
-  }).then(function (result) {
-    let Data = result.map(function (item) {
-      return item;
-
-    });
-    if (Data.length > 0) {
-      callback(Data);
-    } else {
-      callback(undefined);
-    }
-
-  }).catch(function (result) { //catching any then errors
-    console.log("Error " + result);
-    callback(undefined);
-  });*/
 }
