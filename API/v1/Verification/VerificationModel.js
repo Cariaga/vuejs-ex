8ce17fc55953ba99fb48de07c19529075c2e219b
Kaivaan <cariaga.info@gmail.com>
2018-09-11 11:08:26 +0800

    asf

--- API/v1/Verification/VerificationModel.js
+++ API/v1/Verification/VerificationModel.js
@@ -13,9 +13,7 @@ let DBConnect = require("../../SharedController/DBConnect");
  * @param {*} callback
  */
 module.exports.VerifyAccount = function VerifyAccount(UserName, ValidKey, callback) { // Verification with ValidKey // Public only use // Via ValidKey
-  let query =
-    `SET @UserName=${UserName};` +
-    `SET @ValidKey=${ValidKey};` +
+  let query ="";
 
     DBConnect.DBConnect(query, function (response) {
       if (response != undefined) {
@@ -25,82 +23,4 @@ module.exports.VerifyAccount = function VerifyAccount(UserName, ValidKey, callba
         callback(undefined);
       }
     });
-  /*Models.UserAccount.update({
-      Verify: true
-    }, {
-      where: {
-        UserName: UserName,
-        ValidKey: ValidKey
-      }
-    })
-    .then(Success => {
-      callback("Updated");
-    })
-
-    .catch(error => {
-      console.log("Error Updating " + error);
-      callback();
-    });*/
-}
-
-/**
- *
- *
- * @param {*} UserName
- * @param {*} ValidKey
- * @param {*} callback
- */
-module.exports.Verify = function Verify(UserName, ValidKey, callback) {
-  let query =
-    `SET @UserName=${UserName};` +
-    `SET @ValidKey=${ValidKey};` +
-    async.waterfall([
-      myFirstFunction,
-      mySecondFunction,
-    ], function (err, result) { //final function
-      // result now equals 'done'
-      // console.log('5');
-      callback(result);
-    });
-
-  function myFirstFunction(callback2) {
-    console.log('1');
-    Models.UserAccount.sync( /*{force:true}*/ ); //makes sure table exist and syncs it
-    let result = Models.UserAccount.findAll({
-      where: {
-        UserName: UserName //not null
-          ,
-        ValidKey: ValidKey //not null
-      }
-    }).then(function (result) {
-      let Data = result.map(function (item) {
-        return item;
-      });
-      //  console.log('2');
-      callback2(null, Data);
-    }).catch(function (result2) {
-      console.log("Verify Error : " + result2);
-      //  console.log('2');
-      callback2(null, result2);
-    });
-
-  }
-
-  function mySecondFunction(arg1, callback3) {
-    //  console.log(arg1);
-    //  console.log('3'+arg1[0].Verify);
-    if (arg1[0].Verify == true) {
-      let result3 = {
-        Verified: true
-      };
-      //  console.log('4');
-      callback3(null, result3);
-    } else {
-      let result3 = {
-        Verified: false
-      };
-      //  console.log('4');
-      callback3(null, result3);
-    }
-  }
 }
