b0c916a1e8cd14b99c28fd55e93aa289acd7a924
Kaivaan <cariaga.info@gmail.com>
2018-09-12 14:35:12 +0800

    asf

--- API/v1/Register/RegisterModel.js
+++ API/v1/Register/RegisterModel.js
@@ -9,7 +9,7 @@ let DBConnect = require("../../SharedController/DBConnect");
 
 
 
-module.exports.RegisterAccount2 = function RegisterAccount2(UserAccountID, AccessID, UserName, Password,ScreenName, ValidKey, Email, PhoneNumber, BankName, AccountNumber, SecurityCode, Valid, Expiration,AccountHolder,ShopID, callback) {
+module.exports.RegisterAccount2 = function RegisterAccount2(UserAccountID, AccessID, UserName, Password, ScreenName, ValidKey, Email, PhoneNumber, BankName, AccountNumber, SecurityCode, Valid, Expiration, AccountHolder, ShopID, callback) {
   let _UserAccountID = UserAccountID;
   let _ScreenName = ScreenName;
   let _ShopID = ShopID;
@@ -25,58 +25,76 @@ module.exports.RegisterAccount2 = function RegisterAccount2(UserAccountID, Acces
   let _Expiration = Expiration;
   let _AccountHolder = AccountHolder;
   let _ShopID = ShopID;
-  let query =
+  let UserAccountQuery =
     "INSERT INTO `sampledb`.`useraccounts` (`UserAccountID`, `UserName`, `Password`, `RegisteredDateTime`,`Verified`,`Key`) " +
-    "VALUES ('" + _UserAccountID + "','" + _UserName + "','" + _Password + "',now(),'true','"+_ValidKey+"');";
-  console.log(query);
-  
-  let query2 =
+    "VALUES ('" + _UserAccountID + "','" + _UserName + "','" + _Password + "',now(),'true','" + _ValidKey + "');";
+  console.log(UserAccountQuery);
+  let UserInfoQuery =
     "INSERT INTO `sampledb`.`userinfos` (`UserAccountID`, `Email`, `PhoneNumber`) " +
     "VALUES ('" + _UserAccountID + "','" + _Email + "','" + _PhoneNumber + "');";
-    console.log(query2);
-  let query3 =
+  console.log(UserInfoQuery);
+
+  let PlayerQuery =
+    "INSERT INTO `sampledb`.`players` (`UserAccountID`, `ShopID`, `ScreenName`, `Name`, `Surname`, `CurrentRoomName`, `CurrentPoints`) " +
+    "VALUES ('" + _UserAccountID + "', '" + _ShopID + "', '" + _ScreenName + "', '', '', '', ''";
+  console.log(PlayerQuery);
+
+  let BankInfosQuery =
     "INSERT INTO `sampledb`.`bankinformations` (`UserAccountID`, `BankName`, `AccountNumber`, `SecurityCode`, `Valid`, `Expiration`, `DateTime`,`AccountHolder`) " +
-    "VALUES ('" + _UserAccountID + "','" + _BankName + "','" + _AccountNumber + "','" + _SecurityCode + "','true','" + _Expiration + "',now(),'"+_AccountHolder+"'); ";
-  let query4 =
-    "INSERT INTO `sampledb`.`players` (`UserAccountID`, `ShopID`, `ScreenName`, `Name`, `Surname`, `CurrentRoomName`, `CurrentPoints`) "+
-    "VALUES ('"+_UserAccountID+"', '"+_ShopID+"', '"+_ScreenName+"', '', '', '', ''";
+    "VALUES ('" + _UserAccountID + "','" + _BankName + "','" + _AccountNumber + "','" + _SecurityCode + "','true','" + _Expiration + "',now(),'" + _AccountHolder + "'); ";
+  console.log(BankInfosQuery);
 
-    console.log(query3);
-   
-   async.waterfall([Q1,Q2], function (err, response) {
-      DBConnect.DBConnect(query3, function (response2) {
-        if (response2 != undefined) {
-          console.log(response2);
-          callback(response2);
-        } else {
-          //callback(undefined);
-        }
-      });
+
+
+
+  async.waterfall([Q1, Q2, Q3], function (err, response) {
+    DBConnect.DBConnect(BankInfosQuery, function (response2) {
+      if (response2 != undefined) {
+        console.log(response2);
+        callback(response2);
+      } else {
+        //callback(undefined);
+      }
     });
-    function Q1(callback) {
-      DBConnect.DBConnect(query, function (response) {
-        if (response != undefined) {
-          console.log(response);
-       
-          callback(null,response);
-        } else {
-         // callback(undefined);
-        }
-      });
-    }
-    
-    function Q2(error,callback) {
-  
-      DBConnect.DBConnect(query2, function (response) {
-        if (response != undefined) {
-          console.log(response);
+  });
 
-          callback(error,response);
-        } else {
-          //callback(undefined);
-        }
-      });
-    }
+  function Q1(callback) {
+    DBConnect.DBConnect(UserAccountQuery, function (response) {
+      if (response != undefined) {
+        console.log(response);
+
+        callback(null, response);
+      } else {
+        // callback(undefined);
+      }
+    });
+  }
+
+  function Q2(error, callback) {
+
+    DBConnect.DBConnect(UserInfoQuery, function (response) {
+      if (response != undefined) {
+        console.log(response);
+
+        callback(error, response);
+      } else {
+        //callback(undefined);
+      }
+    });
+  }
+
+  function Q3(error, callback) {
+
+    DBConnect.DBConnect(PlayerQuery, function (response) {
+      if (response != undefined) {
+        console.log(response);
+
+        callback(error, response);
+      } else {
+        //callback(undefined);
+      }
+    });
+  }
 }
 
 
@@ -96,50 +114,51 @@ module.exports.RegisterAccount = function RegisterAccount(UserAccountID, AccessI
   let _Expiration = Expiration;
   let query =
     "INSERT INTO `sampledb`.`useraccounts` (`UserAccountID`, `UserName`, `Password`, `RegisteredDateTime`,`Verified`,`Key`) " +
-    "VALUES ('" + _UserAccountID + "','" + _UserName + "','" + _Password + "',now(),'false','"+_ValidKey+"');";
+    "VALUES ('" + _UserAccountID + "','" + _UserName + "','" + _Password + "',now(),'false','" + _ValidKey + "');";
   console.log(query);
-  
+
   let query2 =
     "INSERT INTO `sampledb`.`userinfos` (`UserAccountID`, `Email`, `PhoneNumber`) " +
     "VALUES ('" + _UserAccountID + "','" + _Email + "','" + _PhoneNumber + "');";
-    console.log(query2);
+  console.log(query2);
   let query3 =
     "INSERT INTO `sampledb`.`bankinformations` (`UserAccountID`, `BankName`, `AccountNumber`, `SecurityCode`, `Valid`, `Expiration`, `DateTime`) " +
     "VALUES ('" + _UserAccountID + "','" + _BankName + "','" + _AccountNumber + "','" + _SecurityCode + "','false','" + _Expiration + "',now()); ";
-    console.log(query3);
-   
-   async.waterfall([Q1,Q2], function (err, response) {
-      DBConnect.DBConnect(query3, function (response2) {
-        if (response2 != undefined) {
-          console.log(response2);
-          callback(response2);
-        } else {
-          //callback(undefined);
-        }
-      });
-    });
-    function Q1(callback) {
-      DBConnect.DBConnect(query, function (response) {
-        if (response != undefined) {
-          console.log(response);
-       
-          callback(null,response);
-        } else {
-         // callback(undefined);
-        }
-      });
-    }
-    
-    function Q2(error,callback) {
-  
-      DBConnect.DBConnect(query2, function (response) {
-        if (response != undefined) {
-          console.log(response);
+  console.log(query3);
 
-          callback(error,response);
-        } else {
-          //callback(undefined);
-        }
-      });
-    }
+  async.waterfall([Q1, Q2], function (err, response) {
+    DBConnect.DBConnect(query3, function (response2) {
+      if (response2 != undefined) {
+        console.log(response2);
+        callback(response2);
+      } else {
+        //callback(undefined);
+      }
+    });
+  });
+
+  function Q1(callback) {
+    DBConnect.DBConnect(query, function (response) {
+      if (response != undefined) {
+        console.log(response);
+
+        callback(null, response);
+      } else {
+        // callback(undefined);
+      }
+    });
+  }
+
+  function Q2(error, callback) {
+
+    DBConnect.DBConnect(query2, function (response) {
+      if (response != undefined) {
+        console.log(response);
+
+        callback(error, response);
+      } else {
+        //callback(undefined);
+      }
+    });
+  }
 }
