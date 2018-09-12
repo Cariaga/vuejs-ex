04be971f9397fee6747744c5a7844217928f766d
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-12 14:43:07 +0800

    u

--- API/v1/Distributor/DistributorModel.js
+++ API/v1/Distributor/DistributorModel.js
@@ -15,15 +15,15 @@ let DBConnect = require("../../SharedController/DBConnect");
  * @param {*} callback
  */
 module.exports.AddDistributor = function AddDistributor(UserAccountID, HeadOfficeID, Name, callback) {
-  let query ="";
-    DBConnect.DBConnect(query, function (response) {
-      if (response != undefined) {
-        console.log(response);
-        callback(response);
-      } else {
-        callback(undefined);
-      }
-    });
+  let query = "";
+  DBConnect.DBConnect(query, function (response) {
+    if (response != undefined) {
+      console.log(response);
+      callback(response);
+    } else {
+      callback(undefined);
+    }
+  });
 
   /*var item1 = Models.Distributor.build({
     UserAccountID: UserAccountID,
@@ -61,17 +61,17 @@ module.exports.DistributorUpdate = function DistributorUpdate(UserAccountID, Hea
     `SET @UserAccountID=${UserAccountID};` +
     `SET @HeadOfficeID=${HeadOfficeID};` +
     `SET @Name=${Name};` +
-    "UPDATE `sampledb`.`distributors` "+
-    "SET  HeadOfficeID = @HeadOfficeID , Name=@Name"+
+    "UPDATE `sampledb`.`distributors` " +
+    "SET  HeadOfficeID = @HeadOfficeID , Name=@Name" +
     "WHERE HeadOfficeID=@HeadOfficeID and UserAccountID = @UserAccountID;";
-    DBConnect.DBConnect(query, function (response) {
-      if (response != undefined) {
-        console.log(response);
-        callback(response);
-      } else {
-        callback(undefined);
-      }
-    });
+  DBConnect.DBConnect(query, function (response) {
+    if (response != undefined) {
+      console.log(response);
+      callback(response);
+    } else {
+      callback(undefined);
+    }
+  });
   /*Models.Distributor.update({
       UserAccountID: UserAccountID,
       HeadOfficeID: HeadOfficeID,
@@ -90,65 +90,67 @@ module.exports.DistributorUpdate = function DistributorUpdate(UserAccountID, Hea
       callback(undefined);
     });*/
 
-    module.exports.RegisterDistributor = function RegisterDistributor(UserAccountID,Name,PhoneNumber,UserName,Password,Commission,HeadOfficeID,callback){
-      let _UserAccountID = UserAccountID;
-      let _Name = Name;
-      let _PhoneNumber = PhoneNumber;
-      let _UserName = UserName;
-      let _Password = Password;
-      let _Commission = Commission;
-      let _HeadOfficeID = HeadOfficeID;
-    
-      function Q1(){
-        let query = "INSERT INTO `sampledb`.`useraccounts` (`UserAccountID`, `UserName`, `Password`, `RegisteredDateTime`, `OnlineStatus`, `Verified`, `Key`) "+
-        " VALUES ('"+_UserAccountID+"', '"+_UserName+"', '"+_Password+"', now(), 'Offline', 'true', null);";
-        return new Promise(resolve => {
-          DBConnect.DBConnect(query, function (response) {
-            if (response != undefined) {
-              console.log(response);
-              resolve(response);
-            } else {
-              resolve(undefined);
-            }
-          });
+  module.exports.RegisterDistributor = function RegisterDistributor(UserAccountID, Name, PhoneNumber, UserName, Password, Commission, HeadOfficeID, callback) {
+    let _UserAccountID = UserAccountID;
+    let _Name = Name;
+    let _PhoneNumber = PhoneNumber;
+    let _UserName = UserName;
+    let _Password = Password;
+    let _Commission = Commission;
+    let _HeadOfficeID = HeadOfficeID;
+
+    function Q1() {
+      let query = "INSERT INTO `sampledb`.`useraccounts` (`UserAccountID`, `UserName`, `Password`, `RegisteredDateTime`, `OnlineStatus`, `Verified`, `Key`) " +
+        " VALUES ('" + _UserAccountID + "', '" + _UserName + "', '" + _Password + "', now(), 'Offline', 'true', null);";
+      return new Promise(resolve => {
+        DBConnect.DBConnect(query, function (response) {
+          if (response != undefined) {
+            console.log(response);
+            resolve(response);
+          } else {
+            resolve(undefined);
+          }
         });
-      }
-      function Q2(){
-        let query ="INSERT INTO `sampledb`.`userinfos` (`UserAccountID`, `Email`, `PhoneNumber`, `TelephoneNumber`) "+
-        "VALUES ('"+_UserAccountID+"', null, '"+_PhoneNumber+"', null);";
-        return new Promise(resolve => {
-          DBConnect.DBConnect(query, function (response) {
-            if (response != undefined) {
-              console.log(response);
-              resolve(response);
-            } else {
-              resolve(undefined);
-            }
-          });
-        });
-      }
-      function Q3(){
-        let query = "INSERT INTO `sampledb`.`distributors` (`UserAccountID`,`Name`,`Commission`,`HeadOfficeID`) VALUES ('"+_UserAccountID+"', '"+_Name+"', '"+_Commission+"','"+_HeadOfficeID+"');";
-        return new Promise(resolve => {
-          DBConnect.DBConnect(query, function (response) {
-            if (response != undefined) {
-              console.log(response);
-              resolve(response);
-            } else {
-              resolve(undefined);
-            }
-          });
-        });
-      }
-      async function RunAsync() {
-        console.log('calling');
-        let finalresult = [{}];
-        let result = await Q1();
-        let result2 = await Q2();
-        let result3 = await Q3();
-        console.log('Done');
-        callback('done');
-      }
-      RunAsync();
+      });
     }
+
+    function Q2() {
+      let query = "INSERT INTO `sampledb`.`userinfos` (`UserAccountID`, `Email`, `PhoneNumber`, `TelephoneNumber`) " +
+        "VALUES ('" + _UserAccountID + "', null, '" + _PhoneNumber + "', null);";
+      return new Promise(resolve => {
+        DBConnect.DBConnect(query, function (response) {
+          if (response != undefined) {
+            console.log(response);
+            resolve(response);
+          } else {
+            resolve(undefined);
+          }
+        });
+      });
+    }
+
+    function Q3() {
+      let query = "INSERT INTO `sampledb`.`distributors` (`UserAccountID`,`Name`,`Commission`,`HeadOfficeID`) VALUES ('" + _UserAccountID + "', '" + _Name + "', '" + _Commission + "','" + _HeadOfficeID + "');";
+      return new Promise(resolve => {
+        DBConnect.DBConnect(query, function (response) {
+          if (response != undefined) {
+            console.log(response);
+            resolve(response);
+          } else {
+            resolve(undefined);
+          }
+        });
+      });
+    }
+    async function RunAsync() {
+      console.log('calling');
+      let finalresult = [{}];
+      let result = await Q1();
+      let result2 = await Q2();
+      let result3 = await Q3();
+      console.log('Done');
+      callback('done');
+    }
+    RunAsync();
+  }
 }
