95c33c18cf36c3046391ada0a87864bf10bc1309
Kaivaan <cariaga.info@gmail.com>
2018-09-12 13:30:13 +0800

    asf

--- API/v1/Register/Register.js
+++ API/v1/Register/Register.js
@@ -7,8 +7,7 @@ var isNullOrEmpty = require('is-null-or-empty');
 var uuidv4 = require('uuid/v4');
 var http = require('http');
 module.exports = function (app) {
-//http://localhost:8080/Api/v1/Register/Add/UserName/1UserName/Password/1Password/Name/1Name/SurName/1SurName/Email/1Email/PhoneNumber/1PhoneNumber/BankName/1BankName/SecurityCode/1SecurityCode/Valid/1Valid/Expiration/1Expiration/AccountNumber/1AccountNumber
-  app.get('/Api/v1/Register/Add/UserName/:UserName/Password/:Password/Name/:Name/SurName/:SurName/Email/:Email/PhoneNumber/:PhoneNumber/BankName/:BankName/SecurityCode/:SecurityCode/Valid/:Valid/Expiration/:Expiration/AccountNumber/:AccountNumber/', function (req, res) {
+  app.get('/Api/v1/Register/Add/UserName/:UserName/Password/:Password/Name/:Name/SurName/:SurName/Email/:Email/PhoneNumber/:PhoneNumber/BankName/:BankName/SecurityCode/:SecurityCode/Expiration/:Expiration/AccountNumber/:AccountNumber/', function (req, res) {
   let Name = req.params.Name;
   let SurName = req.params.SurName;
   let UserName = req.params.UserName;
