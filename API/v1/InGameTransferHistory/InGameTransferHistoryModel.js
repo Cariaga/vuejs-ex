09e82a5a42459b7b18073b591935a57b5ff658bb
Kaivaan <cariaga.info@gmail.com>
2018-09-13 14:11:24 +0800

    asf

---
+++ API/v1/InGameTransferHistory/InGameTransferHistoryModel.js
@@ -0,0 +1,13 @@
+let DBConnect = require("../../SharedController/DBConnect");
+module.exports.SupportTicketUserAccountID = function SupportTicketUserAccountID(UserAccountID, callback) {
+    let _UserAccountID = UserAccountID;
+    let query ="SELECT * FROM sampledb.supporttickets as ST where ST.UserAccountID = '"+_UserAccountID+"' and ST.Status='Pending';";
+      DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+          console.log(response);
+          callback(response);
+        } else {
+          callback(undefined);
+        }
+      });
+  }
