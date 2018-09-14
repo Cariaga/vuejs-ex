b3d5e0b1d5c05df5b1b84c763ba6046bdd121b7d
Kaivaan <cariaga.info@gmail.com>
2018-09-14 09:51:40 +0800

    asf

--- API/v1/InGameDeposit/InGameDeposit.js
+++ API/v1/InGameDeposit/InGameDeposit.js
@@ -7,6 +7,9 @@ var isNullOrEmpty = require('is-null-or-empty');
 
 module.exports = function (app) {
     app.get('/Api/v1/InGameDeposit/Request/UserAccountID/:UserAccountID/Name/:Name/Amount/:Amount/', function (req, res) {
+        let UserAccountID = req.params.UserAccountID;
+        let Name =  req.params.Name;
+        let Amount =  req.params.Amount;
         InGameDepositModel.InGameDeposit(UserAccountID,Name,Amount,function(response){
             res.send(response);
         });
