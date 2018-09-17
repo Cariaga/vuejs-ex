8ea6c7f7b2a46675670dedd486700a1f293accd4
Kaivaan <cariaga.info@gmail.com>
2018-09-17 15:53:36 +0800

    asf

--- API/v1/Poker/Poker.js
+++ API/v1/Poker/Poker.js
@@ -2,9 +2,11 @@ var Combinatorics = require('js-combinatorics');
 const PokerHand = require('poker-hand-evaluator');
 const sortBy = require('sort-array');
 module.exports = function (app) {
+    //https://janrueval.herokuapp.com/poker/"QC","4C","5S","7C","KH","2S","6C"
     app.get('/Api/v1/Poker/:Hand/', (req, res) => {
         let PlayerHand = req.params.Hand;
         let ArrayHand = JSON.parse("[" + PlayerHand + "]"); // to array
+        res.send(ArrayHand);
         let cmb = Combinatorics.combination(ArrayHand, 7); //5 for holdem 7 for omha
         let AllCombinations = [];
         let a;
@@ -17,7 +19,9 @@ module.exports = function (app) {
         }
         let bestScore = sortBy(EvaluatedHand, 'score');
         res.send(bestScore);
+
       });
+
       app.get('/Api/v1/Omaha/:Hand/', (req, res) => {
         let PlayerHand = req.params.Hand;
         let ArrayHand = JSON.parse("[" + PlayerHand + "]"); // to array
