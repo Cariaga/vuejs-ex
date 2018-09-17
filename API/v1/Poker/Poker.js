ef39b84ac29ecdbfbe4ae9ce784473b1ac165ef5
Kaivaan <cariaga.info@gmail.com>
2018-09-17 18:02:38 +0800

    asf

--- API/v1/Poker/Poker.js
+++ API/v1/Poker/Poker.js
@@ -1,8 +1,14 @@
 let PokerModel = require("./PokerModel");
 module.exports = function (app) {
     //https://janrueval.herokuapp.com/poker/"QC","4C","5S","7C","KH","2S","6C"
-    app.get('/Api/v1/Poker/:Hand/', (req, res) => {
+    app.get('/Api/v1/Poker/:Hand0?/:Hand1?/:Hand2?/:Hand3?/:Hand4?/:Hand5?/:Hand6?/:Hand7?/:Hand8?/:Hand9?', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
+        let PlayerHand = req.params.Hand;
+        let PlayerHand = req.params.Hand;
+        let PlayerHand = req.params.Hand;
+        let PlayerHand = req.params.Hand;
+        let PlayerHand = req.params.Hand;
+        let PlayerHand = req.params.Hand;
         let PlayerHand = req.params.Hand;
         let TotalCards  = 5;
         let bestScore = PokerModel.PokerHandCompute(PlayerHand, TotalCards);
