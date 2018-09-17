8de9b9f0610451a93f88feb82bdb5d8d00801053
Kaivaan <cariaga.info@gmail.com>
2018-09-17 16:36:36 +0800

    asf

--- API/v1/Poker/Poker.js
+++ API/v1/Poker/Poker.js
@@ -6,40 +6,36 @@ module.exports = function (app) {
     app.get('/Api/v1/Poker/:Hand/', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
         let PlayerHand = req.params.Hand;
+        let TotalCards  = 5;
+        let bestScore = PokerHandCompute(PlayerHand, TotalCards);
+
         
-        let ArrayHand = JSON.parse("[" + PlayerHand + "]"); // to array
-        let cmb = Combinatorics.combination(ArrayHand, 5); //5 for holdem 7 for omha
-        let AllCombinations = [];
-        let a;
-        while (a = cmb.next()) {
-          AllCombinations.push(a);
-        }
-        let EvaluatedHand = [];
-        let length =AllCombinations.length;
-        for (let i = 0; i < length; ++i) {
-          EvaluatedHand.push(new PokerHand(AllCombinations[i].join().replace(/\,/ig, " "))); //join = tostring() // replacing "," to " " and i = ignore case sensitive, g = global
-        }
-        let bestScore = sortBy(EvaluatedHand, 'score');
         res.send(bestScore);
       });
 
       app.get('/Api/v1/Omaha/:Hand/', (req, res) => {
         let PlayerHand = req.params.Hand;
         let ArrayHand = JSON.parse("[" + PlayerHand + "]"); // to array
-      
-        let cmb = Combinatorics.combination(ArrayHand, 7); //5 for holdem 7 for omha
-        let AllCombinations = [];
-        let a;
-        while (a = cmb.next()) {
-          AllCombinations.push(a);
-        }
-        let EvaluatedHand = [];
-        let length =AllCombinations.length;
-        for (let i = 0; i <length; ++i) {
-          EvaluatedHand.push(new PokerHand(AllCombinations[i].join().replace(/\,/ig, " "))); //join = tostring() // replacing "," to " " and i = ignore case sensitive, g = global
-        }
-        let bestScore = sortBy(EvaluatedHand, 'score');
+        let TotalCards  = 5;
+        let bestScore = PokerHandCompute(PlayerHand, TotalCards);
         res.send(bestScore);
       });
  }
+
+function PokerHandCompute(PlayerHand, TotalCards) {
+    let ArrayHand = JSON.parse("[" + PlayerHand + "]"); // to array
+    let cmb = Combinatorics.combination(ArrayHand, TotalCards); //5 for holdem 7 for omha
+    let AllCombinations = [];
+    let a;
+    while (a = cmb.next()) {
+        AllCombinations.push(a);
+    }
+    let EvaluatedHand = [];
+    let length = AllCombinations.length;
+    for (let i = 0; i < length; ++i) {
+        EvaluatedHand.push(new PokerHand(AllCombinations[i].join().replace(/\,/ig, " "))); //join = tostring() // replacing "," to " " and i = ignore case sensitive, g = global
+    }
+    let bestScore = sortBy(EvaluatedHand, 'score');
+    return bestScore;
+}
 
