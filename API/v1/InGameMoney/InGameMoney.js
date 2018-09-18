8d68f6b4598c8ea2b624eea150da83afcbf14970
Kaivaan <cariaga.info@gmail.com>
2018-09-18 17:12:37 +0800

    asf

---
+++ API/v1/InGameMoney/InGameMoney.js
@@ -0,0 +1,18 @@
+//Profile route has a similar property for profile in game 
+//but this one is used for BuyIn UI amount in game 
+
+let DBConnect = require("../../SharedController/DBConnect");
+let DBCheck = require("../../SharedController/DBCheck");
+let GlobalFunctions = require("../../SharedController/GlobalFunctions");
+let InGameMoneyModel = require("./InGameMoneyModel");
+var beautify = require("json-beautify");
+var isNullOrEmpty = require('is-null-or-empty');
+
+module.exports = function (app) {
+    app.get('/Api/v1/InGameMoney/UserAccountID/:UserAccountID/', function (req, res) {
+        let UserAccountID = req.params.UserAccountID;
+        InGameMoneyModel.Money(UserAccountID, function (response) {
+            res.send(response);
+        });
+    });
+}
---
+++ API/v1/InGameMoney/InGameMoneyModel.js
@@ -0,0 +1,22 @@
+var uuidv4 = require('uuid/v4');
+var passwordValidator = require('password-validator');
+var validator = require('validator'); //email,mobile phone,isIP,isPostalCode,credit card
+var async = require("async");
+var moment = require('moment');
+const Collection = require('linqjs');
+let DBConnect = require("../../SharedController/DBConnect");
+
+module.exports.Money = function Money(UserAccountID, callback) {
+    let _UserAccountID = UserAccountID;
+    let query =
+        "";
+
+    DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+            console.log(response);
+            callback(response);
+        } else {
+            callback(undefined);
+        }
+    });
+}
