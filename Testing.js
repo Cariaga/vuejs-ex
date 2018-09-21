374ad38d0b96f5a687180ceae62d9d3767d58d17
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-09-21 13:35:31 +0800

    asd

--- Testing.js
+++ Testing.js
@@ -326,3 +326,7 @@ DBConnect.RegisterAccount2('UserAccountID', 'AccessID', 'UserName','Password', '
 //http://192.168.254.106:8080/Api/v1/Dashboard/HeadOffice/Betting/Monthly/ - monthly betting
 
 //http://192.168.254.106:8080/Api/v1/Dashboard/HeadOffice/Betting/Yearly/ - yearly betting
+
+//http://192.168.254.106:8080/Api/v1/WithdrawHistoryList/Search/Column/name/Value/n2/StartDate/2018-09-14%2015:18:19/EndDate/2018-09-14%2015:18:19
+
+//http://192.168.254.106:8080/Api/v1/DepositList/Search/Column/name/Value/n2/StartDate/2018-09-14%2015:18:19/EndDate/2018-09-14%2015:18:19
