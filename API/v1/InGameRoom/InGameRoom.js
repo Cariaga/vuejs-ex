843ef691cb81582d9fd19e98cc1b3ee78fc045b9
Kaivaan <cariaga.info@gmail.com>
2018-09-21 12:05:09 +0800

    asf

--- API/v1/InGameRoom/InGameRoom.js
+++ API/v1/InGameRoom/InGameRoom.js
@@ -8,8 +8,8 @@ let http = require('http');
 module.exports = function (app) {
     app.get('/Api/v1/InGameRoom/Update/UserAccountID/:UserAccountID/RoomName/:RoomName/', function (req, res) {
         let UserAccountID = req.params.UserAccountID;
-        let UserAccountID = req.params.RoomName;
-        InGameScoreModel.WinRate(UserAccountID, function (response) {
+        let RoomName = req.params.RoomName;
+        InGameScoreModel.InGameRoomPlayerCurrentRoom(UserAccountID,RoomName, function (response) {
             res.send(response);
         });
     });
