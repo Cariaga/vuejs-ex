9d86a2902a721f41e44f78537f68e28a28a403a5
Kaivaan <cariaga.info@gmail.com>
2018-09-24 17:36:41 +0800

    asf

--- API/v1/Register/Register.js
+++ API/v1/Register/Register.js
@@ -7,7 +7,7 @@ var isNullOrEmpty = require('is-null-or-empty');
 var uuidv4 = require('uuid/v4');
 var http = require('http');
 module.exports = function (app) {
-  
+
   app.get('/Api/v1/Register/Add/ScreenName/:ScreenName/UserName/:UserName/Password/:Password/Name/:Name/SurName/:SurName/Email/:Email/PhoneNumber/:PhoneNumber/BankName/:Bankname/SecurityCode/:SecurityCode/AccountNumber/:AccountNumber/AccountHolder/:AccountHolder/ShopID/:ShopID/', function (req, res) {
     let ScreenName = req.params.ScreenName;
     let UserName = req.params.UserName;
@@ -17,38 +17,38 @@ module.exports = function (app) {
     let BankName = req.params.BankName;
     let AccountNumber = req.params.AccountNumber;
     let SecurityCode = req.params.SecurityCode;
-  
+
     let AccountHolder = req.params.AccountHolder;
     let ShopID = req.params.ShopID;
     let UserAccountID = uuidv4();
     let ValidKey = uuidv4();
     let AccessID = "1";
     let Valid = '';
-    let Expiration ='';
+    let Expiration = '';
     //newer version
-    if (!isNullOrEmpty(ScreenName)){
-      if (!isNullOrEmpty(ScreenName)){
-        if (!isNullOrEmpty(ScreenName)){
-          if (!isNullOrEmpty(ScreenName)){
-            if (!isNullOrEmpty(ScreenName)){
-    RegisterModel.RegisterAccount2(UserAccountID, AccessID, UserName,Password, ScreenName, ValidKey, Email, PhoneNumber, BankName, AccountNumber, SecurityCode, Valid, Expiration, AccountHolder, '1', function (response) {
-      if (response != undefined) {
-        // res.send(response);
-        let status = 200;
-        res.status(status).end(http.STATUS_CODES[status]);
-      } else {
-        res.send({
-          AddUserAccountFailed: true
-        });
-      }
-    });
-  }
-}
+    if (!isNullOrEmpty(ScreenName)) {
+      if (!isNullOrEmpty(ScreenName)) {
+        if (!isNullOrEmpty(ScreenName)) {
+          if (!isNullOrEmpty(ScreenName)) {
+            if (!isNullOrEmpty(ScreenName)) {
+              RegisterModel.RegisterAccount2(UserAccountID, AccessID, UserName, Password, ScreenName, ValidKey, Email, PhoneNumber, BankName, AccountNumber, SecurityCode, Valid, Expiration, AccountHolder, ShopID, function (response) {
+                if (response != undefined) {
+                  // res.send(response);
+                  let status = 200;
+                  res.status(status).end(http.STATUS_CODES[status]);
+                } else {
+                  res.send({
+                    AddUserAccountFailed: true
+                  });
+                }
+              });
+            }
+          }
         }
       }
     }
 
-  
+
   });
 
 
