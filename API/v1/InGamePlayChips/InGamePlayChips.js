9b55eff4c7e29cf16bccc46cfaa644f5de9d2def
Kaivaan <cariaga.info@gmail.com>
2018-09-24 20:25:07 +0800

    asf

--- API/v1/InGamePlayChips/InGamePlayChips.js
+++ API/v1/InGamePlayChips/InGamePlayChips.js
@@ -8,12 +8,17 @@ let http = require('http');
 module.exports = function (app) {
     app.get('/Api/v1/InGamePlayChips/Add/UserAccountID/:UserAccountID/SeasonID/:SeasonID/Chips/:Chips', function (req, res) {
         let UserAccountID = req.params.UserAccountID;
+        let SeasonID = req.params.SeasonID;
         let Chips = req.params.Chips;
-        
+
         if(!isNullOrEmpty(UserAccountID)){
-            InGamePlayChipsModel.AddChipsInSeason(UserAccountID,SeasonID,Points, function (response) {
-            res.send(response);
-        });
+            if(!isNullOrEmpty(UserAccountID)){
+                if(!isNullOrEmpty(Chips)){
+                    InGamePlayChipsModel.AddChipsInSeason(UserAccountID,SeasonID,Points, function (response) {
+                        res.send(response);
+                        });
+                }
+            }
         }
     });
 }
