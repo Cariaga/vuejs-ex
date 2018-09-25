af9dab58b610415f54587e03fd3017bc43a1dd40
Kaivaan <cariaga.info@gmail.com>
2018-09-25 11:27:11 +0800

    asf

--- API/v1/InGameTransferRequest/InGameTransferRequest.js
+++ API/v1/InGameTransferRequest/InGameTransferRequest.js
@@ -10,12 +10,23 @@ var isNullOrEmpty = require('is-null-or-empty');
 let InGameTransferRequestModel = require('./InGameTransferRequestModel');
 let http = require('http');
 module.exports = function(app){
-    app.get('/Api/v1/InGameTransferHistory/UserAccountIDSender/:UserAccountIDSender/UserAccountIDReciver/:UserAccountIDReciver/Amount/:Amount/Reason/:Reason/', function (req, res) {
+    app.get('/Api/v1/InGameTransferHistoryRequest/UserAccountIDSender/:UserAccountIDSender/UserAccountIDReciver/:UserAccountIDReciver/Amount/:Amount/Reason/:Reason/', function (req, res) {
       let UserAccountIDSender = req.params.UserAccountIDSender;
       let UserAccountIDReciver = req.params.UserAccountIDReciver;
       let Reason = req.params.Reason;
       let Amount = req.params.Amount;
-      
+ 
+      var promise = new Promise(function(resolve, reject) {
+        
+       });
+       Promise.all([promise]).then(function(response) {
+        callback(response);
+        }, function(){ //if promise fail
+        console.log('something went wrong')
+        callback(undefined);
+      });
+
+
       InGameTransferRequestModel.RequestTransferHistory(UserAccountIDSender,UserAccountIDReciver,Amount,Reason,function(response){
           res.send(response);
       });
