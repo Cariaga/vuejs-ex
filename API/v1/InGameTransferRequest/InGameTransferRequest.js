d721e2d5b5783f2523fe6ad370c7ef2880af30ad
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-10-05 15:33:27 +0800

    up

--- API/v1/InGameTransferRequest/InGameTransferRequest.js
+++ API/v1/InGameTransferRequest/InGameTransferRequest.js
@@ -123,4 +123,118 @@ module.exports = function (app) {
             });
         }
     });
+
+
+    app.get('/Api/v1/InGameTransferHistoryRequest/UserAccountIDSender/:UserAccountIDSender/UserName/:UserName/Amount/:Amount/', function (req, res) {
+        let UserAccountIDSender = req.params.UserAccountIDSender;
+        let UserName = req.params.UserName;
+        let Amount = req.params.Amount;
+
+        if (!isNullOrEmpty(UserAccountIDSender)) {
+            if (!isNullOrEmpty(Amount)) {
+                if (validator.isNumeric(Amount)) {
+                    InGameTransferRequestModel.UserNameUserAccount(UserName, function (response) {
+                        if (response != undefined) {
+                            let UserAccountIDReceiver = response[0].UserAccountID;
+                            var promise = new Promise(function (resolve, reject) {
+                                InGameTransferRequestModel.PlayerNewMoneySubtract(UserAccountIDSender, Amount, function (response) {
+                                    if (response != undefined) {
+                                        //console.log("Valid Value "+response.NewMoney>=0);
+                                        if (response.NewMoney >= 0) {
+                                            resolve(response);
+                                        } else {
+                                            reject({
+                                                NotEnoughSenderPoints: true
+                                            });
+                                        }
+                                    } else {
+                                        reject();
+                                    }
+                                });
+                            });
+                            var promise2 = new Promise(function (resolve, reject) {
+                                InGameTransferRequestModel.PlayerNewMoneyAdd(UserAccountIDReceiver, Amount, function (response) {
+                                    if (response != undefined) {
+                                        resolve(response);
+                                    } else {
+                                        reject();
+                                    }
+                                });
+                            });
+                            Promise.all([promise, promise2]).then(function (response) {
+                                let NewMoneyOfSender = response[0].NewMoney;
+                                let NewMoneyOfReciever = response[1].NewMoney;
+
+                                var promise3 = new Promise(function (resolve, reject) {
+                                    InGameTransferRequestModel.UpdatePlayerMoney(UserAccountIDSender, NewMoneyOfSender, function (response) { //sender
+                                        if (response != undefined) {
+                                            resolve();
+                                        } else {
+                                            reject({
+                                                FailedUpdatePlayerSender: true
+                                            });
+                                        }
+                                    });
+
+                                });
+                                var promise4 = new Promise(function (resolve, reject) {
+                                    InGameTransferRequestModel.UpdatePlayerMoney(UserAccountIDReceiver, NewMoneyOfReciever, function (response) { //reciever
+                                        if (response != undefined) {
+                                            resolve();
+                                        } else {
+                                            reject({
+                                                FailedUpdatePlayerReciever: true
+                                            });
+                                        }
+                                    });
+
+                                });
+
+                                Promise.all([promise3, promise4]).then(function (response) {
+                                    if (response != undefined) {
+                                        InGameTransferRequestModel.RequestTransferHistory(UserAccountIDSender, UserAccountIDReceiver, Amount, function (response) {
+                                            if (response != undefined) {
+                                                let status = 200;
+                                                res.status(status).end(http.STATUS_CODES[status]);
+                                            } else {
+                                                res.send({
+                                                    FailedRequestTransferHistory: true
+                                                })
+                                            }
+                                        });
+
+                                    } else {
+
+                                    }
+
+                                }, function (error) {
+                                    res.send(error);
+                                });
+
+                            }, function (error) {
+                                res.send(error);
+                            });
+                        } else {
+                            let status = 404;
+                            res.status(status).end(http.STATUS_CODES[status]);
+                        }
+
+                    });
+
+                } else {
+                    res.send({
+                        InvalidAmount: true
+                    });
+                }
+            } else {
+                res.send({
+                    NoAmount: true
+                });
+            }
+        } else {
+            res.send({
+                NoUserAccountIDSender: true
+            });
+        }
+    });
 }
