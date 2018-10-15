8ce08f36f1d968d5ffaf28d5652284ea69e6bb54
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-10-15 16:14:29 +0800

    up

---
+++ API/v1/InGameBuyIn/InGameBuyIn.js
@@ -0,0 +1,28 @@
+let DBConnect = require("../../SharedController/DBConnect");
+let DBCheck = require("../../SharedController/DBCheck");
+let GlobalFunctions = require("../../SharedController/GlobalFunctions");
+let InGameBuyInModel = require("./InGameBuyInModel");
+var beautify = require("json-beautify");
+var isNullOrEmpty = require('is-null-or-empty');
+let http = require('http');
+var Security = require('../../SharedController/Security');
+module.exports = function (app) {
+
+    app.get('/Api/v1/InGameBuyIn/UserAccountID/:UserAccountID/BuyInAmount/:BuyInAmount',Security.verifyToken, function (req, res) {
+        let UserAccountID = req.params.UserAccountID;
+        let BuyInAmount = req.params.UserAccountID;
+        if (!isNullOrEmpty(UserAccountID)) {
+            DBCheck.isUserAccountIDExist(UserAccountID, function (response) {
+                if (response == true) {
+                    InGameBuyInModel.BuyInChips(UserAccountID,BuyInAmount, function (response) {
+                        res.send(response);
+                    });
+                } else {
+                    let status = 404;
+                    res.status(status).end(http.STATUS_CODES[status]);
+                }
+            });
+        }
+    });
+
+}
---
+++ API/v1/InGameBuyIn/InGameBuyInModel.js
@@ -0,0 +1,24 @@
+var uuidv4 = require('uuid/v4');
+var passwordValidator = require('password-validator');
+var validator = require('validator'); //email,mobile phone,isIP,isPostalCode,credit card
+var async = require("async");
+var moment = require('moment');
+const Collection = require('linqjs');
+let DBConnect = require("../../SharedController/DBConnect");
+
+module.exports.BuyInChips = function BuyInChips(UserAccountID, callback) {
+    let _UserAccountID = UserAccountID;
+    let _BuyInAmount = BuyInAmount;
+    let query =
+        "UPDATE sampledb.player_profile" + 
+        "SET Money = Money - '"+_BuyInAmount+"' " + 
+        "WHERE UserAccountID = '"+_UserAccountID+"'";
+    DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+           // console.log(response);
+            callback(response);
+        } else {
+            callback(undefined);
+        }
+    });
+}
