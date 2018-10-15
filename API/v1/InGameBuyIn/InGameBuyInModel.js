4a767c864beac7d2387df0718ee5b0039f96818b
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-10-15 16:27:44 +0800

    Update API/v1/InGameBuyIn/InGameBuyInModel.js

--- API/v1/InGameBuyIn/InGameBuyInModel.js
+++ API/v1/InGameBuyIn/InGameBuyInModel.js
@@ -6,16 +6,16 @@ var moment = require('moment');
 const Collection = require('linqjs');
 let DBConnect = require("../../SharedController/DBConnect");
 
-module.exports.BuyInChips = function BuyInChips(UserAccountID, callback) {
+module.exports.BuyInChips = function BuyInChips(UserAccountID, BuyInAmount, callback) {
     let _UserAccountID = UserAccountID;
     let _BuyInAmount = BuyInAmount;
     let query =
-        "UPDATE sampledb.player_profile" + 
-        "SET Money = Money - '"+_BuyInAmount+"' " + 
-        "WHERE UserAccountID = '"+_UserAccountID+"'";
+        "UPDATE sampledb.player_profile" +
+        "SET Money = Money - '" + _BuyInAmount + "' " +
+        "WHERE UserAccountID = '" + _UserAccountID + "'";
     DBConnect.DBConnect(query, function (response) {
         if (response != undefined) {
-           // console.log(response);
+            // console.log(response);
             callback(response);
         } else {
             callback(undefined);
