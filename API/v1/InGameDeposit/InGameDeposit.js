512d89c0c0365e1d206356eb0549bcb9567c191a
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-10-18 14:57:30 +0800

    Update API/v1/InGameDeposit/InGameDeposit.js

--- API/v1/InGameDeposit/InGameDeposit.js
+++ API/v1/InGameDeposit/InGameDeposit.js
@@ -7,35 +7,53 @@ var isNullOrEmpty = require('is-null-or-empty');
 let http = require('http');
 var Security = require('../../SharedController/Security');
 module.exports = function (app) {
-    function InGameDeposit(UserAccountID,Name,Amount,res){
-        DBCheck.isUserAccountIDExist(UserAccountID, function (response) {
-            if (response == true) {
-                InGameDepositModel.InGameDeposit(UserAccountID, Name, Amount, function (response) {
-                    if(response!=undefined){
-                        let status = 200;
-                        res.status(status).end(http.STATUS_CODES[status]);
-                    }else{
-                        let status = 404;
-                        res.status(status).end(http.STATUS_CODES[status]);
-                    }
-                });
+    function InGameDeposit(UserAccountID, Name, Amount, res) {
+        if (!isNullOrEmpty(UserAccountID)) {
+            if (!isNullOrEmpty(Name)) {
+                if (!isNullOrEmpty(Amount)) {
+                    DBCheck.isUserAccountIDExist(UserAccountID, function (response) {
+                        if (response == true) {
+                            InGameDepositModel.InGameDeposit(UserAccountID, Name, Amount, function (response) {
+                                if (response != undefined) {
+                                    let status = 200;
+                                    res.status(status).end(http.STATUS_CODES[status]);
+                                } else {
+                                    let status = 404;
+                                    res.status(status).end(http.STATUS_CODES[status]);
+                                }
+                            });
+                        } else {
+                            let status = 404;
+                            res.status(status).end(http.STATUS_CODES[status]);
+                        }
+                    });
+                } else {
+                    res.send({
+                        NoAmount: true
+                    });
+                }
             } else {
-                let status = 404;
-                res.status(status).end(http.STATUS_CODES[status]);
+                res.send({
+                    NoName: true
+                });
             }
-        });
+        } else {
+            res.send({
+                NoUserAccountID: true
+            });
+        }
     }
 
-    app.post('/Api/v1/InGameDeposit/Request/',Security.verifyToken, function (req, res) {
+    app.post('/Api/v1/InGameDeposit/Request/', Security.verifyToken, function (req, res) {
         let UserAccountID = req.body.UserAccountID;
         let Name = req.body.Name;
         let Amount = req.body.Amount;
-        InGameDeposit(UserAccountID,Name,Amount,res);
+        InGameDeposit(UserAccountID, Name, Amount, res);
     });
-    app.get('/Api/v1/InGameDeposit/Request/UserAccountID/:UserAccountID/Name/:Name/Amount/:Amount/',Security.verifyToken, function (req, res) {
+    app.get('/Api/v1/InGameDeposit/Request/UserAccountID/:UserAccountID/Name/:Name/Amount/:Amount/', Security.verifyToken, function (req, res) {
         let UserAccountID = req.params.UserAccountID;
         let Name = req.params.Name;
         let Amount = req.params.Amount;
-        InGameDeposit(UserAccountID,Name,Amount,res);
+        InGameDeposit(UserAccountID, Name, Amount, res);
     });
 }
