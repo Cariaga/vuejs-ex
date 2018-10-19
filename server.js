ebcc90c7be5521f8d8774bab4a6034656981eed5
theklayd <clydesantos1028@gmail.com>
2018-10-19 14:26:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -222,8 +222,8 @@ const server = app
   .listen(8080, () => console.log(`Listening on ${ 8080 }`));
 
 const wss = new SocketServer({ server });
-let ConnectedUsers=0;
 let ClientList=[];
+let ConnectedUsers=0;
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -237,7 +237,9 @@ wss.on('connection', (ws,req) => {
     ws.UserAccountID = parameters.query.UserAccountID;
   
     ws.Money = getRandomInt(100,10000);
-  
+
+
+    console.log('useraccount : '+ws.UserAccountID)
 
 
 
@@ -247,10 +249,9 @@ wss.on('connection', (ws,req) => {
     //event retrive and update room money
 
     //remove room money link when player leaves
-  ConnectedUsers++;
-
-  console.log('Client connected '+ConnectedUsers);
-
+    ConnectedUsers++;
+    console.log('Client connected '+ConnectedUsers);
+    
   ws.onmessage = function(event) {
    /* var UserAccountID = event.data;
     var PlayerFound = ClientList.filter(e => e.UserAccountID === UserAccountID)[0];
@@ -264,15 +265,17 @@ wss.on('connection', (ws,req) => {
       console.log("Not Found So We Add It");
       ClientList.push({UserAccountID:UserAccountID})
     }
-*/
+  */ 
+    
     console.debug("WebSocket message received:", event.data);
-
-
+    // console.log(ClientList.length)
+   // ClientList[0].send('hello I received it');
    // console.debug("WebSocket message received:", event);
   };
   ws.onerror = function(event) {
     
     console.debug("WebSocket Error message received:", event);
+
   };
   ws.onclose = function(event) {
     ConnectedUsers--;
