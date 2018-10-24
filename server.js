7a6083611d158754564d5f8f854731429e615cc2
Kaivaan <cariaga.info@gmail.com>
2018-10-24 17:14:05 +0800

    asf

--- server.js
+++ server.js
@@ -227,7 +227,7 @@ const server = app
   .use((req, res) => res.send(""));
   
 server.listen(8080, () => console.log(`Listening on ${ 8080 }`));
-
+/*
 const wss = new SocketServer({
   server
 });
@@ -440,7 +440,7 @@ setInterval(() => {
         },null,0));
       }
   });*/
-}, 1000);
+/*}, 1000);
 
 function IsJsonString(str) {
   try {
@@ -458,7 +458,7 @@ function IsJsonString(str) {
     return false;
   }
   return true;
-}
+}*/
 
 // listen (start app with node server.js) ======================================
 //server.listen(port, ip);
