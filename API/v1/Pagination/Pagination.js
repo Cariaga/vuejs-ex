796f6e62dcea3cc5cf191e1ddfbbbd2bf4674d91
Kaivaan <cariaga.info@gmail.com>
2018-10-25 13:48:05 +0800

    asf

--- API/v1/Pagination/Pagination.js
+++ API/v1/Pagination/Pagination.js
@@ -2,17 +2,22 @@ let PaginationModel = require('./PaginationModel');
 let http = require('http');
 var Security = require('../../SharedController/Security');
 module.exports = function (app) { //SELECTION
-    /*app.get('/Api/v1/Pagination/',Security.verifyToken, function (req, res) {
+    app.get('/Api/v1/Pagination/',Security.verifyToken, function (req, res) {
         PaginationModel.PaginationCount(function(response){
             res.send(response);
         });
-    });*/
+    });
     app.get('/Api/v1/Pagination/', function (req, res) {
         PaginationModel.PaginationCount(function(response){
             res.send(response);
         });
     });
     app.post('/Api/v1/Pagination/',Security.verifyToken, function (req, res) {
+        PaginationModel.PaginationCount(function(response){
+            res.send(response);
+        });
+    });
+    app.post('/Api/v1/Pagination/', function (req, res) {
         PaginationModel.PaginationCount(function(response){
             res.send(response);
         });
