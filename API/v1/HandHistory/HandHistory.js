32bc2c86f5fbae735cdad56f912ad6283bc64a6b
Kaivaan <cariaga.info@gmail.com>
2018-10-26 13:08:00 +0800

    asf

--- API/v1/HandHistory/HandHistory.js
+++ API/v1/HandHistory/HandHistory.js
@@ -25,7 +25,7 @@ module.exports = function (app) { //MODIFY
                       AddHandHistoryFailed: true
                     });
                   }
-
+                  
                 });
               } else {
                 res.send({
