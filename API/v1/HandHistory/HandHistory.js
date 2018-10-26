1c9f8df0d9b44876582fccb386bf3ab03365944e
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-10-26 14:17:50 +0800

    Update API/v1/HandHistory/HandHistory.js

--- API/v1/HandHistory/HandHistory.js
+++ API/v1/HandHistory/HandHistory.js
@@ -12,6 +12,7 @@ module.exports = function (app) { //MODIFY
     if (!isNullOrEmpty(SeasonID)) {
       if (!isNullOrEmpty(UserAccountID)) {
         if (!isNullOrEmpty(MoveHand)) {
+          if (!isNullOrEmpty(Amount)) {
           if (MoveHand == "Fold" || MoveHand == "Call" || MoveHand == "Raise" || MoveHand == "Check" ||MoveHand =="TimedOut" || MoveHand=="AllIn") {
             DBCheck.isUserAccountIDExist(UserAccountID, function (response) {
               if (response == true) {
@@ -33,6 +34,11 @@ module.exports = function (app) { //MODIFY
                 });
               }
             });
+          }else{
+            res.send({
+              AmountInvalidValue: true
+            });
+          }
 
           } else {
             res.send({
