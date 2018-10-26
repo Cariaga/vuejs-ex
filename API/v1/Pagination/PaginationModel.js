ee3bffafe427fe829698d112891ba7e321001ba8
theklayd <clydesantos1028@gmail.com>
2018-10-26 16:39:02 +0800

    pagination

--- API/v1/Pagination/PaginationModel.js
+++ API/v1/Pagination/PaginationModel.js
@@ -1,7 +1,21 @@
 let DBConnect = require("../../SharedController/DBConnect");
 
 module.exports.PaginationCount = function PaginationCount(callback) {
-    let query ="SELECT * FROM sampledb.paginationcount";
+    let page = [
+      'deposit_list',
+      'withdraw_list',
+      'notifications',
+      'player_supportlist',
+      'useraccounts',
+      'player_iplist',
+      'blacklist',
+      'gamelog_list',
+      'loginhistory_list',
+      'member_list',
+      'headoffice_list'
+    ]
+
+    let query ="SELECT count(*) FROM sampledb."+page[0];
     DBConnect.DBConnect(query,function(response){
       if(response!=undefined){
         // console.log(response);
