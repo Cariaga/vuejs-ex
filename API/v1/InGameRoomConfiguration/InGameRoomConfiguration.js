067c20e8517f958a998c9e841360d7d259cdaec3
Kaivaan <cariaga.info@gmail.com>
2018-10-30 19:17:59 +0800

    FilterBuyin

--- API/v1/InGameRoomConfiguration/InGameRoomConfiguration.js
+++ API/v1/InGameRoomConfiguration/InGameRoomConfiguration.js
@@ -18,20 +18,39 @@ module.exports = function (app) {
         if (!isNullOrEmpty(UserAccountID)) {
             DBCheck.isUserAccountIDExist(UserAccountID, function (response) {
                 if (response == true) {
-                    let RoomID=  UUID();//1 pass to check 
-                    DBCheck.IsRoomIDExist(RoomID, function (response) {
+                    function getRandomInt(min, max) {
+                        return Math.floor(Math.random() * (max - min + 1)) + min;
+                    }
+                    function GenerateUUID() {
+                        res.send(UUID());
+                      }
+                      setTimeout(GenerateUUID, getRandomInt(10,200));
+                }
+                   // if (response == false) {
+
+                       
+
+                   /* DBCheck.IsRoomIDExist(RoomID, function (response) {
                         if (response == false) {
-                            res.send(UUID());
+                            function GenerateUUID() {
+                                res.send(UUID());
+                              }
+                              setTimeout(GenerateUUID, 200);
                         } else {
-                            res.send(UUID());//2 pass to check again
+                            function GenerateUUID() {
+                                res.send(UUID());
+                              }
+                              setTimeout(GenerateUUID, 200);
+
+                           // res.send(UUID());//2 pass to check again
                            /* let status = 404;
                             res.status(status).end(http.STATUS_CODES[status]);*/
-                        }
-                    });
-                }else{
+                   // }*/
+                   // });
+              /*  }else{
                     let status = 404;
                     res.status(status).end(http.STATUS_CODES[status]);
-                }
+                }*/
             });
         } else {
             let status = 404;
