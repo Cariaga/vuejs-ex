29f98f373b35a365c4edd1228d3ed799e5565858
tristansalao <39991524+tristansalao@users.noreply.github.com>
2018-11-07 16:22:07 +0800

    up

--- API/v1/Sales/SalesModel.js
+++ API/v1/Sales/SalesModel.js
@@ -12,3 +12,30 @@ module.exports.SalesLowRank = function SalesLowRank(HeadOfficeID, callback){
           }
     });
 }
+
+module.exports.SalesLowRankDistributor = function SalesLowRankDistributor(DistributorID, callback){
+    let _DistributorID = DistributorID;
+    let query = "SELECT * FROM sampledb.lowrank_dis WHERE DistributorID = '"+_DistributorID+"';";
+    console.log(query);
+    DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+            callback(response);
+          } else {
+            callback(undefined);
+          }
+    });
+}
+
+module.exports.SalesLowRankShop = function SalesLowRankShop(ShopID, callback){
+    let _ShopID = ShopID;
+    let query = "SELECT * FROM sampledb.lowrank_shop WHERE ShopID = '"+_ShopID+"';";
+    console.log(query);
+    DBConnect.DBConnect(query, function (response) {
+        if (response != undefined) {
+            callback(response);
+          } else {
+            callback(undefined);
+          }
+    });
+}
+
