10ed3a153606c1f387a15edc8d5d02166f05cdfd
Kaivaan <cariaga.info@gmail.com>
2018-11-12 19:47:02 +0800

    response

--- API/v1/InGameSeason/InGameSeason.js
+++ API/v1/InGameSeason/InGameSeason.js
@@ -42,7 +42,8 @@ module.exports = function (app) { //selection
     app.get('/Api/v1/InGameSeason/SeasonEnd/SeasonID/:SeasonID/', Security.verifyToken, function (req, res) {
         let SeasonID = req.params.SeasonID;
         if(!isNullOrEmpty(SeasonID)){
-            
+            let status = 200;
+            res.status(status).end(http.STATUS_CODES[status]);
         }
     });
 
