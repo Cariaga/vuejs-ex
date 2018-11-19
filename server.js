dc399e46ca2add2d64e98d8bf8da569cc11434ed
Kaivaan <cariaga.info@gmail.com>
2018-11-19 21:37:45 +0800

    asf

--- server.js
+++ server.js
@@ -157,8 +157,8 @@ require('./API/v1/InGameSeason/InGameSeason')(app);
 require('./API/v1/InGameRoomConfiguration/InGameRoomConfiguration')(app);
 require('./API/v1/InGamePlayerWins/InGamePlayerWins')(app);
 require('./API/v1/InGameBuyIn/InGameBuyIn')(app);
-/*require('./API/v1/Sales/Sales')(app);
-require('./API/v1/UserProfit/UserProfit')(app);
+require('./API/v1/Sales/Sales')(app);
+/*require('./API/v1/UserProfit/UserProfit')(app);
 */
 
 function test() {
