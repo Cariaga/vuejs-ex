0870e8002284f3e9ea5b4f4fd21bf549eb229a1a
Kaivaan <cariaga.info@gmail.com>
2018-11-22 11:53:43 +0800

    asf

--- API/v1/DashBoard/DashBoard.js
+++ API/v1/DashBoard/DashBoard.js
@@ -6,49 +6,44 @@ let http = require('http');
 var Security = require('../../SharedController/Security');
 module.exports = function (app) { 
 
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-    app.get('/Api/v1/Dashboard/HeadOffice/Betting/Daily/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+
+    app.get('/Api/v1/Dashboard/HeadOffice/Betting/Daily/',/* Security.rateLimiterMiddleware,*/Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.HeadOfficeBettingDaily(function (response) {
             res.send(response);
           });
       });
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-    app.get('/Api/v1/Dashboard/HeadOffice/Betting/Monthly/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+    app.get('/Api/v1/Dashboard/HeadOffice/Betting/Monthly/', /*Security.rateLimiterMiddleware,*/Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.HeadOfficeBettingMonthly(function (response) {
             res.send(response);
           });
       });
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-    app.get('/Api/v1/Dashboard/HeadOffice/Betting/Yearly/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+    app.get('/Api/v1/Dashboard/HeadOffice/Betting/Yearly/', /*Security.rateLimiterMiddleware,*/Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.HeadOfficeBettingYearly(function (response) {
             res.send(response);
           });
       });
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-    app.get('/Api/v1/Dashboard/HeadOffice/Deposit/Daily/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+    app.get('/Api/v1/Dashboard/HeadOffice/Deposit/Daily/', /*Security.rateLimiterMiddleware,*/Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.HeadOfficeDepositDaily(function (response) {
             res.send(response);
           });
       });
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-    app.get('/Api/v1/Dashboard/HeadOffice/Withdraw/Daily/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+    app.get('/Api/v1/Dashboard/HeadOffice/Withdraw/Daily/', /*Security.rateLimiterMiddleware,*/Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.HeadOfficeWithdrawDaily(function (response) {
             res.send(response);
           });
       });
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-    app.get('/Api/v1/Dashboard/OnlineStatuses/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+
+    app.get('/Api/v1/Dashboard/OnlineStatuses/', /*Security.rateLimiterMiddleware,*/Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.UserAccountOnline(function (response) {
             res.send(response);
           });
       });
-
     /*app.get('/Api/v1/Dashboard/NewUsers/', function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.UserAccountRecentRegistered(function (response) {
@@ -56,35 +51,35 @@ module.exports = function (app) {
           });
       });*/
 
-    app.get('/Api/v1/Dashboard/TotalRegisteredUser/', /*Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),*/ function (req, res) {
+    app.get('/Api/v1/Dashboard/TotalRegisteredUser/', /*Security.rateLimiterMiddleware*/,Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.TotalRegisteredUsers(function (response) {
             res.send(response[0]);
           });
       });
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-    app.get('/Api/v1/Dashboard/TotalRegisteredUsersToday/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+
+    app.get('/Api/v1/Dashboard/TotalRegisteredUsersToday/',/* Security.rateLimiterMiddleware*/,Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.TotalRegisteredUsersToday(function (response) {
             res.send(response[0]);
             });
         });
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-      app.get('/Api/v1/Dashboard/TotalDepositWithdrawProfit/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+
+      app.get('/Api/v1/Dashboard/TotalDepositWithdrawProfit/', /*Security.rateLimiterMiddleware*/,Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.TotalWithdrawDepositProfit(function (response) {
             res.send(response);
           });
       });
-// Security.rateLimiterMiddleware,Security.verifyToken,Security.cache.route({ expire: 5  }),
-      app.get('/Api/v1/Dashboard/TransactionRecent/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+
+      app.get('/Api/v1/Dashboard/TransactionRecent/', /*Security.rateLimiterMiddleware*/,Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         DashBoardModel.TransactionRecent(function (response) {
             res.send(response);
           });
       });
-      app.get('/Api/v1/Dashboard/TotalTransactionRecent/', Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
-        res.setHeader('Content-Type',Security.verifyToken,Security.cache.route({ expire: 5  }),  'application/json');
+      app.get('/Api/v1/Dashboard/TotalTransactionRecent/',Security.verifyToken,Security.cache.route({ expire: 5  }), function (req, res) {
+        res.setHeader('Content-Type', 'application/json');
         DashBoardModel.TotalTransactionRecent(function (response) {
             res.send(response);
           });
