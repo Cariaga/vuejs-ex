9a57624ff98a5b105cde2a3cf432fd438870d01c
Kaivaan <cariaga.info@gmail.com>
2018-12-05 11:04:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -253,7 +253,7 @@ app.get('/GameVersion/',Security.rateLimiterMiddleware,Security.cache.route({ ex
 
 
 app.get('/success', function (req, res) {
-  console.log(req);
+  console.log("Respnsed "+req.params.MyShopParam3);
   res.sendStatus(200);
 });
 app.get('/fail', function (req, res) {
