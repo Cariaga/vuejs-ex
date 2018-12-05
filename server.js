961ff906bf68e09a055836c73578c45cb3e00a44
Kaivaan <cariaga.info@gmail.com>
2018-12-05 16:07:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -329,12 +329,12 @@ app.get('/Pay2/DepositAmount/:DepositAmount', function (req, res) {
   };
 
   //let computedsignature = w1.getSignature(fields);
-  let fields = w1.getFormFields(feildvalues);
-  signature = w1.getSignature(fields);
 
+  signature = w1.getSignature(fields);
   feildvalues.WMI_SUCCESS_URL = 'https://tester-holdem-server.4b63.pro-ap-southeast-2.openshiftapps.com/success/'+signature;
   feildvalues.WMI_FAIL_URL= 'https://tester-holdem-server.4b63.pro-ap-southeast-2.openshiftapps.com/fail/';
   
+  let fields = w1.getFormFields(feildvalues);
 
   var createInput = function(name, value){
     return '<input name="' + name + '" value="' + value + '">';
