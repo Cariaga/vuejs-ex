e36634df42b1ff92cc449105aa10732278e6c4e7
Kaivaan <cariaga.info@gmail.com>
2018-12-05 16:24:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -317,7 +317,7 @@ app.get('/Pay2/DepositAmount/:DepositAmount', function (req, res) {
   let PaymentNumber = getRandomInt(0,10000);
   w1.setAlgorithm("md5");
   let feildvalues ={
-    WMI_SUCCESS_URL = 'https://tester-holdem-server.4b63.pro-ap-southeast-2.openshiftapps.com/success/',
+    WMI_SUCCESS_URL = "https://tester-holdem-server.4b63.pro-ap-southeast-2.openshiftapps.com/success/",
     WMI_PAYMENT_AMOUNT: DepositAmount,
     WMI_PAYMENT_NO:PaymentNumber,
     WMI_CURRENCY_ID: '840',
