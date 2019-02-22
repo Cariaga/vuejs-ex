dec047d4ecd1a6993c21db9273329e794f56ef1e
theklayd <clydesantos1028@gmail.com>
2019-02-22 18:00:48 +0800

    xxxx

--- API/v1/Photon/Photon.js
+++ API/v1/Photon/Photon.js
@@ -8,7 +8,8 @@ let http = require('http');
 var Security = require('../../SharedController/Security');
 var Management = require('../../SharedController/Management');
 
-let photonvariables = []
+let photonvariables = [];
+
 module.exports = function (app) {
 
   app.post('/Api/v1/photon', function (req, res) {
@@ -23,43 +24,43 @@ module.exports = function (app) {
 
   app.post('/Api/v1/photon/close', function (req, res) {
       console.log(Object.values(req.params));
-        var status = 200;photonvariables.push(Object.values(req.params));
-        res.status(status).end(http.STATUS_CODES[status]);
+      photonvariables.push(Object.values(req.params));
+        res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/create', function (req, res) {
       console.log(Object.values(req.params));
-        var status = 200;photonvariables.push(Object.values(req.params));
-        res.status(status).end(http.STATUS_CODES[status]);
+      photonvariables.push(Object.values(req.params));
+        res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/event', function (req, res) {
       console.log(Object.values(req.params));
-        var status = 200;photonvariables.push(Object.values(req.params));
-        res.status(status).end(http.STATUS_CODES[status]);
+      photonvariables.push(Object.values(req.params));
+        res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/properties', function (req, res) {
       console.log(Object.values(req.params));
-        var status = 200;photonvariables.push(Object.values(req.params));
-        res.status(status).end(http.STATUS_CODES[status]);
+      photonvariables.push(Object.values(req.params));
+        res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/join', function (req, res) {
       console.log(Object.values(req.params));
-        var status = 200;photonvariables.push(Object.values(req.params));
-        res.status(status).end(http.STATUS_CODES[status]);
+      photonvariables.push(Object.values(req.params));
+        res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/leave', function (req, res) {
       console.log(Object.values(req.params));
       photonvariables.push(Object.values(req.params));
-        var status = 200;
-        res.status(status).end(http.STATUS_CODES[status]);
+    
+        res.sendStatus(200);
   });
 
-  app.post('/Api/v1/photon/sample', function (req, res) {
-      res.send(photonvariables.length);
+  app.get('/Api/v1/photon/sample', function (req, res) {
+      res.send({sample : photonvariables.length});
   });
 
 
