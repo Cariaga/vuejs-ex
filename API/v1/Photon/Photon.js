b6a27418e6f9f9f61dc33fb3aca6006d9a464903
theklayd <clydesantos1028@gmail.com>
2019-02-22 13:10:49 +0800

    remove token on api/v1/photon

--- API/v1/Photon/Photon.js
+++ API/v1/Photon/Photon.js
@@ -9,9 +9,39 @@ var Security = require('../../SharedController/Security');
 var Management = require('../../SharedController/Management');
 module.exports = function (app) {
 
-  app.post('/Api/v1/photon', function (req, res) {
+  app.get('/Api/v1/photon', function (req, res) {
+      console.log('you called photon')
+      console.log(Object.values(req.params))
+      res.send('ok')
+  });
+
+  app.post('/Api/v1/photon/close', function (req, res) {
       console.log(Object.values(req.params))
   });
+
+  app.post('/Api/v1/photon/create', function (req, res) {
+      console.log(Object.values(req.params))
+  });
+
+  app.post('/Api/v1/photon/event', function (req, res) {
+      console.log(Object.values(req.params))
+  });
+
+  app.post('/Api/v1/photon/properties', function (req, res) {
+      console.log(Object.values(req.params))
+  });
+
+  app.post('/Api/v1/photon/join', function (req, res) {
+      console.log(Object.values(req.params))
+  });
+
+  app.post('/Api/v1/photon/leave', function (req, res) {
+      console.log(Object.values(req.params))
+  });
+
+
+
+  
 
   app.post('/Api/v1/photon/close', function (req, res) {
       console.log(Object.values(req.params))
