67733a1995e896aac98618241f36984125576906
theklayd <clydesantos1028@gmail.com>
2019-02-22 17:38:50 +0800

    Update Photon.js

--- API/v1/Photon/Photon.js
+++ API/v1/Photon/Photon.js
@@ -7,12 +7,16 @@ var isNullOrEmpty = require('is-null-or-empty');
 let http = require('http');
 var Security = require('../../SharedController/Security');
 var Management = require('../../SharedController/Management');
+
+let photonvariables = []
 module.exports = function (app) {
 
   app.post('/Api/v1/photon', function (req, res) {
       console.log('you called photon')
-      console.log(Object.values(req.params))
+    //   console.log(Object.values(req.params))
+      photonvariables.push(Object.values(req.params))
       res.send('ok')
+
   });
 
   app.post('/Api/v1/photon/close', function (req, res) {
@@ -37,6 +41,10 @@ module.exports = function (app) {
 
   app.post('/Api/v1/photon/leave', function (req, res) {
       console.log(Object.values(req.params))
+  });
+
+  app.post('/Api/v1/photon/sample', function (req, res) {
+      res.send(photonvariables.length);
   });
 
 
