a6385a8ecfb18f0c763b864f95ba7832d3dd973c
theklayd <clydesantos1028@gmail.com>
2019-02-25 20:54:41 +0800

    854

--- API/v1/Photon/Photon.js
+++ API/v1/Photon/Photon.js
@@ -8,81 +8,85 @@ let http = require('http');
 var Security = require('../../SharedController/Security');
 var Management = require('../../SharedController/Management');
 
-let photonvariables = [];
 let photonActions = [];
-let photonParams = [];
-let keys = []
+let photonBase = []
+let photonClose = [];
+let photonCreate = [];
+let photonEvent = [];
+let photonProperties = [];
+let photonJoin = [];
+let photonLeave = [];
 module.exports = function (app) {
 
   app.post('/Api/v1/photon', function (req, res) {
-      console.log('you called photon')
-    //   console.log('you called photon');
-      photonActions.push('you called photon')
-      keys.push(Object.keys(req.body)[0]);
-      res.send({ "Message" : "ok", "ResultCode" : 0 });
-
+    photonActions.push('base called');
+    photonBase.push(req.body)
+    res.send({ "Message" : "ok", "ResultCode" : 0 });
   });
 
   app.post('/Api/v1/photon/close', function (req, res) {
-      console.log('you called photon');
-      photonActions.push('you called photon/close');
-      photonParams.push(req.body);
-      photonvariables.push(req.body.PathClose);
-      keys.push(Object.keys(req.body)[0]);
-        res.send({ "Message" : "ok", "ResultCode" : 0 });
+    photonActions.push('close called');
+    photonClose.push(req.body);
+    res.send({ "Message" : "ok", "ResultCode" : 0 });
   });
 
   app.post('/Api/v1/photon/create', function (req, res) {
-      console.log('you called photon');
-      photonActions.push('you called photon/create');
-       photonParams.push(req.body);
-      photonvariables.push(req.body.PathCreate);
-      keys.push(Object.keys(req.body)[0]);
-        res.send({ "Message" : "ok", "ResultCode" : 0 });
+    photonActions.push('create called');
+    photonCreate.push(req.body);
+    res.send({ "Message" : "ok", "ResultCode" : 0 });
   });
 
   app.post('/Api/v1/photon/event', function (req, res) {
-      console.log('you called photon');
-      photonActions.push('you called photon/event');
-      keys.push(Object.keys(req.body)[0]);
-        res.send({ "Message" : "ok", "ResultCode" : 0 });
+    photonActions.push('event called');
+    photonEvent.push(req.body);
+    res.send({ "Message" : "ok", "ResultCode" : 0 });
   });
 
   app.post('/Api/v1/photon/properties', function (req, res) {
-      console.log('you called photon');
-      photonActions.push('you called photon/properties');
-      keys.push(Object.keys(req.body)[0]);
-        res.send({ "Message" : "ok", "ResultCode" : 0 });
+    photonActions.push('properties called');
+    photonProperties.push(req.body);
+    res.send({ "Message" : "ok", "ResultCode" : 0 });
   });
 
   app.post('/Api/v1/photon/join', function (req, res) {
-      console.log('you called photon');
-      photonActions.push('you called photon/join');
-      photonvariables.push(req.body.PathJoin);
-      keys.push(Object.keys(req.body)[0]);
-      res.send({ "Message" : "ok", "ResultCode" : 0 });
-
-    });
+    photonActions.push('join called');
+    photonJoin.push(req.body);
+    res.send({ "Message" : "ok", "ResultCode" : 0 });
+  });
     
   app.post('/Api/v1/photon/leave', function (req, res) {
-    console.log('you called photon');
-    photonActions.push('you called photon/leave');
-    
-    keys.push(Object.keys(req.body)[0]);
-      res.send({ "Message" : "ok", "ResultCode" : 0 });
+    photonActions.push('leave called');
+    photonLeave.push(req.body);
+    res.send({ "Message" : "ok", "ResultCode" : 0 });
   });
+
 
   app.get('/Api/v1/photon/getActions', function (req, res) {
       res.send(photonActions);
   });
-  app.get('/Api/v1/photon/getVariables', function (req, res) {
-      res.send(photonvariables);
+
+  app.get('/Api/v1/photon/getClose', function (req, res) {
+      res.send(photonClose);
   });
-  app.get('/Api/v1/photon/getParams', function (req, res) {
-      res.send(photonParams);
+
+  app.get('/Api/v1/photon/getCreate', function (req, res) {
+      res.send(photonCreate);
   });
-  app.get('/Api/v1/photon/getKeys', function (req, res) {
-      res.send(keys);
+
+  app.get('/Api/v1/photon/getEvent', function (req, res) {
+      res.send(photonEvent);
+  });
+
+  app.get('/Api/v1/photon/getProperties', function (req, res) {
+      res.send(photonProperties);
+  });
+
+  app.get('/Api/v1/photon/getJoin', function (req, res) {
+      res.send(photonJoin);
+  });
+
+  app.get('/Api/v1/photon/getLeave', function (req, res) {
+      res.send(photonLeave);
   });
 
 
