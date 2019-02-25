b9801f2f999e7dfa33d86d20f5cecdd2f5c68616
theklayd <clydesantos1028@gmail.com>
2019-02-25 15:45:25 +0800

    345

--- API/v1/Photon/Photon.js
+++ API/v1/Photon/Photon.js
@@ -14,8 +14,8 @@ module.exports = function (app) {
 
   app.post('/Api/v1/photon', function (req, res) {
       console.log('you called photon')
-    //   console.log(JSON.parse(req.params));
-      photonvariables.push(JSON.parse(req.params))
+    //   console.log('you called photon');
+      photonvariables.push('you called photon')
 
       var status = 200;
       res.status(status).end(http.STATUS_CODES[status]);
@@ -23,38 +23,38 @@ module.exports = function (app) {
   });
 
   app.post('/Api/v1/photon/close', function (req, res) {
-      console.log(JSON.parse(req.params));
-      photonvariables.push(JSON.parse(req.params));
+      console.log('you called photon');
+      photonvariables.push('you called photon/close');
         res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/create', function (req, res) {
-      console.log(JSON.parse(req.params));
-      photonvariables.push(JSON.parse(req.params));
+      console.log('you called photon');
+      photonvariables.push('you called photon/create');
         res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/event', function (req, res) {
-      console.log(JSON.parse(req.params));
-      photonvariables.push(JSON.parse(req.params));
+      console.log('you called photon');
+      photonvariables.push('you called photon/event');
         res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/properties', function (req, res) {
-      console.log(JSON.parse(req.params));
-      photonvariables.push(JSON.parse(req.params));
+      console.log('you called photon');
+      photonvariables.push('you called photon/properties');
         res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/join', function (req, res) {
-      console.log(JSON.parse(req.params));
-      photonvariables.push(JSON.parse(req.params));
+      console.log('you called photon');
+      photonvariables.push('you called photon/join');
         res.sendStatus(200);
   });
 
   app.post('/Api/v1/photon/leave', function (req, res) {
-      console.log(JSON.parse(req.params));
-      photonvariables.push(JSON.parse(req.params));
+      console.log('you called photon');
+      photonvariables.push('you called photon/leave');
     
         res.sendStatus(200);
   });
