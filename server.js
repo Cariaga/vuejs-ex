bbc054ff08cf38ccdc3e0ac14be390befa6a1dfe
Kaivaan <cariaga.info@gmail.com>
2019-02-27 21:42:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -400,11 +400,19 @@ wss.on('connection', (ws, req) => {
     ws.UserName = BasicInformation.UserName;
     ws.WinPoints = BasicInformation.WinPoints;
     ws.PlayerCommission = BasicInformation.PlayerCommission;
-    
+
     var query2 = "UPDATE `sampledb`.`useraccounts` SET `OnlineStatus` = 'Online' WHERE (`UserAccountID` = \'"+_UserAccountID+"\');";
     DBConnect.DBConnect(query2, function (response) {
       if (response != undefined) {
-        
+        var _UserAccountID = UserAccountID;
+        var query = "SELECT `Money` FROM sampledb.players WHERE `UserAccountID` = \'" + _UserAccountID + "\';";
+        DBConnect.DBConnect(query, function (response) {
+          if (response != undefined) {
+            ws.Money = parseInt(response[0].Money);
+            
+            //console.log(response[0]);
+          }
+        });
       }
     });
     
@@ -424,6 +432,7 @@ wss.on('connection', (ws, req) => {
 
   //--inisialization to Same Account instances // similar to all buffer
   var SyncRoomVar = undefined;
+
   wss.clients.forEach((client) => {
     if (SyncRoomVar == undefined && client.UserAccountID == ws.UserAccountID) { //matching user account connecting to a diffrent application instance
       SyncRoomVar = client.Rooms;
@@ -437,18 +446,8 @@ wss.on('connection', (ws, req) => {
   }
   //console.log(ws.Money);
 
-  var _UserAccountID = UserAccountID;
-  var query = "SELECT `Money` FROM sampledb.players WHERE `UserAccountID` = \'" + _UserAccountID + "\';";
-  DBConnect.DBConnect(query, function (response) {
-    if (response != undefined) {
-      ws.Money = parseInt(response[0].Money);
-      ParentListOfPlayer();
 
-
-      //console.log(response[0]);
-    }
-  });
-
+  ParentListOfPlayer();
   function ParentListOfPlayer(){
     var ParentsUserAccountsQuery = "SELECT ParentUserAccountID FROM sampledb.player_treebranch_indirect where PlayerUserAccountID=\'"+UserAccountID+"\';";
     //console.log(ParentsUserAccountsQuery);
