a8d5c6050af78a7fc00924c647d33740269bf98d
Kaivaan <cariaga.info@gmail.com>
2019-02-28 15:42:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -322,22 +322,13 @@ client.on("error", function (err) {
 });
  
 client.set("string key", "string val", redis.print);
-client.hset("'"+process.env.OPENSHIFT_BUILD_COMMIT+"'", redis.print);
-
-
+client.hset("hash key", "hashtest 1", "some value", redis.print);
 client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
 client.hkeys("hash key", function (err, replies) {
-  if(replies.length!=undefined){
     console.log(replies.length + " replies:");
-  }else{
-    console.log("test redis empty");
-  }
-
-
     replies.forEach(function (reply, i) {
         console.log("    " + i + ": " + reply);
     });
-
     client.quit();
 });
 
