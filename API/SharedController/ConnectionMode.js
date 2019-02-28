9bb5a23c9a63b010886e479274d1087d5530e4d1
Kaivaan <cariaga.info@gmail.com>
2019-02-28 16:15:08 +0800

    Update ConnectionMode.js

--- API/SharedController/ConnectionMode.js
+++ API/SharedController/ConnectionMode.js
@@ -2,6 +2,10 @@
 let Remote = "https://tester-holdem-server.4b63.pro-ap-southeast-2.openshiftapps.com";
 let localhost = "localhost";
 
-module.exports.tes123 = function (test) {
-    test(true);
+module.exports.getMainAddressByProductionKey = function (callback) {
+    if(process.env.NODE_ENV=="production"){
+        callback(Remote)
+    }else{
+        callback(localhost);
+    }
 }
